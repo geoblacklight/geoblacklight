48796e895322888212d6f32991393f04fc2a82ff
Darren Hardy <drh@stanford.edu>
2014-07-02 12:30:21 -0700

    add geoblacklight-map-view id

--- app/assets/javascripts/geoblacklight/modules/map-view.js
+++ app/assets/javascripts/geoblacklight/modules/map-view.js
@@ -2,8 +2,8 @@
 
 console.log('DEBUG: Inside geoblacklight/modules/map-view.js');
 Blacklight.onLoad(function () {
-  $('#map').geoBlacklight_setupMapView();
-  $('#map').geoBlacklight_setupTools();
+  $('#geoblacklight-map-view').geoBlacklight_setupMapView();
+  $('#geoblacklight-tools').geoBlacklight_setupTools();
 });
 
 /* Requires leaflet */
@@ -32,7 +32,7 @@ Blacklight.onLoad(function () {
 
       /* XXX: externalize configuration for Mapbox application key */
       var basemap = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
         maxZoom: 18
       }).addTo(map);
 
