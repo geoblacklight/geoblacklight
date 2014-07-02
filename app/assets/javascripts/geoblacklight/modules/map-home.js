2987bdff251fa2ab7db65613ec0c1f6e804b9a51
Darren Hardy <drh@stanford.edu>
2014-07-02 12:30:37 -0700

    use mapquest

--- app/assets/javascripts/geoblacklight/modules/map-home.js
+++ app/assets/javascripts/geoblacklight/modules/map-home.js
@@ -13,8 +13,8 @@ Blacklight.onLoad(function () {
     console.log(this)
     return this.each(function () {
       var map = L.map('map');
-      var basemap = L.tileLayer('https://a.tiles.mapbox.com/v3/drh-stanford.ic5mf3lb/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      var basemap = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
         maxZoom: 18
       }).addTo(map);
       map.setZoom(1);
