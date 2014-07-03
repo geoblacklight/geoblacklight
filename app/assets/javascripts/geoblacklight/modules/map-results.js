e84c3b8333478fc04b7c0ccfa1748b48e42f4d56
Darren Hardy <drh@stanford.edu>
2014-07-03 15:07:43 -0700

    use round-robin for tileservers

--- app/assets/javascripts/geoblacklight/modules/map-results.js
+++ app/assets/javascripts/geoblacklight/modules/map-results.js
@@ -12,9 +12,11 @@ Blacklight.onLoad(function () {
     // console.log(this)
       return this.each(function () {
       map = L.map('map');
-      var basemap = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+
+      var basemap = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
-        maxZoom: 18
+        maxZoom: 18,
+        subdomains: '1234' // see http://developer.mapquest.com/web/products/open/map
       }).addTo(map);
       map.setView([0,0],1);
       // console.log(mapBbox);
--- app/assets/javascripts/geoblacklight/modules/map-view.js
+++ app/assets/javascripts/geoblacklight/modules/map-view.js
@@ -30,10 +30,10 @@ Blacklight.onLoad(function () {
                        [solrDoc.solr_ne_pt_0_d, solrDoc.solr_ne_pt_1_d]]);
       }
 
-      /* XXX: externalize configuration for Mapbox application key */
-      var basemap = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+      var basemap = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
-        maxZoom: 18
+        maxZoom: 18,
+        subdomains: '1234' // see http://developer.mapquest.com/web/products/open/map
       }).addTo(map);
 
       map.on('click', function(e){
