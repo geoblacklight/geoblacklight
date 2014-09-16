a579fc6b5b8589a07bb6d7e155b6272fe508513f
Jack Reed <pjreed@stanford.edu>
2014-09-16 06:52:41 -0700

    add spatial query injection into catalog controller

--- lib/geoblacklight.rb
+++ lib/geoblacklight.rb
@@ -2,4 +2,8 @@ require "geoblacklight/engine"
 
 module Geoblacklight
   require 'geoblacklight/config'
+  require 'geoblacklight/controller_override'
+  def self.inject!
+    CatalogController.send(:include, Geoblacklight::ControllerOverride)
+  end
 end
---
+++ spec/lib/geoblacklight/controller_override_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Geoblacklight::ControllerOverride do
+  class GeoblacklightControllerTestClass
+    def params
+      {}
+    end
+  end
+  
+  before(:each) do
+    @fake_controller = GeoblacklightControllerTestClass.new
+    @fake_controller.extend(Geoblacklight::ControllerOverride)
+  end
+  
+  let(:solr_params) { OpenStruct.new }
+  let(:req_params) { OpenStruct.new }
+  
+  describe 'add_spatial_params' do
+    it 'should return the solr_params when no bbox is given' do
+      expect(@fake_controller.add_spatial_params(solr_params, req_params)).to eq solr_params
+    end
+    it 'should return a spatial search if bbox is given' do
+      req_params.bbox = '123'
+      expect(has_spatial_query(solr_params, req_params)).to be_truthy
+    end
+  end
+end
+
+def has_spatial_query(solr_params, req_params)
+  /Intersects\(ENVELOPE/ =~ @fake_controller.add_spatial_params(solr_params, req_params).fq.first
+end
