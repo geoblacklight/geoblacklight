8a8b3eb414e0a1c21bea4b7f8d0eaa86cea47f03
Jack Reed <pjreed@stanford.edu>
2014-11-06 23:10:13 -0500

    replaces ogp images with geoblacklight icon font

--- app/helpers/geoblacklight_helper.rb
+++ app/helpers/geoblacklight_helper.rb
@@ -40,47 +40,17 @@ module GeoblacklightHelper
   end
 
   def layer_type_image(type)
-    case type
-    when "Polygon"
-      image_tag('geoblacklight/type_polygon.png', 'data-toggle' => 'tooltip', title: 'Polygon', :class => 'tooltip-icon')
-    when "Line"
-      image_tag('geoblacklight/type_arc.png', 'data-toggle' => 'tooltip', title: 'Line', :class => 'tooltip-icon')
-    when "Point"
-      image_tag('geoblacklight/type_dot.png', 'data-toggle' => 'tooltip', title: 'Point', :class => 'tooltip-icon')
-    when "Raster"
-      image_tag('geoblacklight/type_raster.png', 'data-toggle' => 'tooltip', title: 'Raster', :class => 'tooltip-icon')
-    when "Paper Map"
-      image_tag('geoblacklight/type_map.png', 'data-toggle' => 'tooltip', title: 'Paper Map', :class => 'tooltip-icon')
-    when "LibraryRecord"
-      content_tag(:i, '', class: 'fa fa-book fa-lg text-muted tooltip-icon',  'data-toggle' => 'tooltip', title: 'Library Record')
-    else
-      ""
-    end
+    content_tag :span, '', class: "geoblacklight-icon geoblacklight-#{type.downcase}"
   end
 
   def layer_institution_image(institution)
-    case institution
-    when /Stanford/
-      image_tag('geoblacklight/src_stanford.png', 'data-toggle' => 'tooltip', title: 'Stanford', :class => 'tooltip-icon')
-    when /Berkeley/
-      image_tag('geoblacklight/src_berkeley.png', 'data-toggle' => 'tooltip', title: 'UC Berkeley', :class => 'tooltip-icon')
-    when /Harvard/
-      image_tag('geoblacklight/src_harvard.png', 'data-toggle' => 'tooltip', title: 'Harvard', :class => 'tooltip-icon')
-    when /MIT/
-      image_tag('geoblacklight/src_mit.png', 'data-toggle' => 'tooltip', title: 'MIT', :class => 'tooltip-icon')
-    when /Tufts/
-      image_tag('geoblacklight/src_tufts.png', 'data-toggle' => 'tooltip', title: 'Tufts', :class => 'tooltip-icon')
-    when /MassGIS/
-      image_tag('geoblacklight/src_massgis.png', 'data-toggle' => 'tooltip', title: 'MassGIS', :class => 'tooltip-icon')
-    else
-      ""
-    end
+    content_tag :span, '', class: "geoblacklight-icon geoblacklight-#{institution.downcase}"
   end
 
   def layer_access_image(access)
     case access
     when 'Restricted'
-      content_tag(:i, '', class: 'fa fa-lock fa-lg text-muted  tooltip-icon', 'data-toggle' => 'tooltip', title: 'Restricted')
+      content_tag(:i, '', class: 'fa fa-lock fa-lg text-muted  tooltip-icon', 'data-toggle' => 'tooltip', title: 'Restricted', style: 'width: 17px;')
     when 'Public'
       content_tag(:i, '', class: 'fa fa-unlock fa-lg text-muted tooltip-icon',  'data-toggle' => 'tooltip', title: 'Public')
     else
