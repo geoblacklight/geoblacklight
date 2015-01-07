acbd8607e5a34218cbaedc1184fdee47ae3ca05d
Eliot Jordan <eliotj@princeton.edu>
2015-01-07 16:26:55 -0500

    add rails-assets as gem source

--- template.rb
+++ template.rb
@@ -2,6 +2,8 @@ gem 'blacklight'
 gem 'geoblacklight', github: 'geoblacklight/geoblacklight'
 gem 'jettywrapper'
 
+add_source "https://rails-assets.org"
+
 run 'bundle install'
 
 generate 'blacklight:install', '--devise'
