2a406866e410be34948ee50181605e0071e09091
Jack Reed <pjreed@stanford.edu>
2015-01-29 11:50:59 -0800

    use boost query rather than q

--- lib/geoblacklight/controller_override.rb
+++ lib/geoblacklight/controller_override.rb
@@ -7,8 +7,8 @@ module Geoblacklight
     
     def add_spatial_params(solr_params, req_params)
       if req_params[:bbox]
-        solr_params[:q] ||= "*"
-        solr_params[:q] += " solr_bbox:\"IsWithin(#{req_params[:bbox]})\"^10"
+        solr_params[:bq] ||= []
+        solr_params[:bq] = ["solr_bbox:\"IsWithin(#{req_params[:bbox]})\"^10"]
         solr_params[:fq] ||= []
         solr_params[:fq] << "solr_bbox:\"Intersects(#{req_params[:bbox]})\""
       end
