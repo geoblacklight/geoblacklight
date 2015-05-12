b448d91aa4cb46a55ddbf0d012a82b4eb092511c
Jack Reed <pjreed@stanford.edu>
2015-05-12 09:37:29 -0700

    add comments, private methods, and tests to Geoblacklight::References

--- lib/geoblacklight/references.rb
+++ lib/geoblacklight/references.rb
@@ -1,12 +1,101 @@
 module Geoblacklight
   # References is a geoblacklight-schema dct:references parser
   class References
-    attr_reader :refs
-    def initialize(document)
+    attr_reader :refs, :reference_field
+    def initialize(document, reference_field = :dct_references_s)
       @document = document
+      @reference_field = reference_field
       @refs = parse_references.map { |ref| Reference.new(ref) }
     end
 
+    ##
+    # Accessor for a document's file format
+    # @return [String] file format for the document
+    def format
+      @document[:dc_format_s]
+    end
+
+    ##
+    # @param [String, Symbol] ref_type
+    # @return [Geoblacklight::Reference]
+    def references(ref_type)
+      @refs.find { |reference| reference.type == ref_type }
+    end
+
+    ##
+    # Preferred download (should be a file download)
+    # @return [Hash, nil]
+    def preferred_download
+      return file_download unless download.blank?
+    end
+
+    ##
+    # Download hash based off of format type
+    # @return [Hash, nil]
+    def downloads_by_format
+      case format
+      when 'Shapefile'
+        vector_download_formats
+      when 'GeoTIFF'
+        geotiff_download_formats
+      when 'ArcGRID'
+        arcgrid_download_formats
+      end
+    end
+
+    ##
+    # Generated download types from wxs services
+    # @return (see #downloads_by_format)
+    def download_types
+      downloads_by_format
+    end
+
+    private
+
+    ##
+    # Parses the references field of a document
+    # @return [Hash]
+    def parse_references
+      if @document[reference_field].nil?
+        {}
+      else
+        JSON.parse(@document[reference_field])
+      end
+    end
+
+    ##
+    # Download hash for a static file download
+    # @return (see #downloads_by_format)
+    def file_download
+      { file_download: download.to_hash }
+    end
+
+    ##
+    # Download hash for a Shapefile file (currently only vector) with a wms and wfs reference
+    # present
+    # @return (see #downloads_by_format)
+    def vector_download_formats
+      if wfs && wms
+        { shapefile: wfs.to_hash, kmz: wms.to_hash, geojson: wfs.to_hash } if wms.present? && wfs.present?
+      end
+    end
+
+    ##
+    # Download hash for a GeoTiff file with a WMS reference present
+    # @return (see #downloads_by_format)
+    def geotiff_download_formats
+      { geotiff: wms.to_hash } if wms.present?
+    end
+
+    ##
+    # Download hash for an ArcGRID file with a WMS reference present
+    # @return (see #downloads_by_format)
+    def arcgrid_download_formats
+      { geotiff: wms.to_hash } if wms.present?
+    end
+
+    ##
+    # Adds a call to references for defined URI keys
     def method_missing(m, *args, &b)
       if Geoblacklight::Constants::URI.key?(m)
         references m
@@ -15,45 +104,5 @@ module Geoblacklight
       end
     end
 
-    def format
-      @document[:dc_format_s]
-    end
-
-    def references(ref_type)
-      @refs.find { |reference| reference.type == ref_type }
-    end
-
-    def parse_references
-      if @document[:dct_references_s].nil?
-        Hash.new
-      else
-        JSON.parse(@document[:dct_references_s])
-      end
-    end
-
-    def preferred_download
-      return file_download unless download.blank?
-    end
-
-    def file_download
-      { file_download: download.to_hash }
-    end
-
-    def downloads_by_format
-      case format
-      when 'Shapefile'
-        if wfs && wms
-          { shapefile: wfs.to_hash, kmz: wms.to_hash, geojson: wfs.to_hash } if wms.present? && wfs.present?
-        end
-      when 'GeoTIFF'
-        { geotiff: wms.to_hash } if wms.present?
-      when 'ArcGRID'
-        { geotiff: wms.to_hash } if wms.present?
-      end
-    end
-
-    def download_types
-      downloads_by_format
-    end
   end
 end
