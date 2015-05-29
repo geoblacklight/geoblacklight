c400b1cc2aa91112cb008a96f29b17b142f953ed
Eliot Jordan <eliotj@princeton.edu>
2015-05-29 18:15:19 -0400

    add feature styles and better layer parsing

--- app/assets/javascripts/geoblacklight/viewers/esrimapservice.js
+++ app/assets/javascripts/geoblacklight/viewers/esrimapservice.js
@@ -1,6 +1,15 @@
 //= require geoblacklight/viewers/map
 
 GeoBlacklight.Viewer.Esrimapservice = GeoBlacklight.Viewer.Map.extend({
+  layerInfo: {},
+
+  // default feature styles
+  defaultStyles: {
+      'esriGeometryPoint': '',
+      'esriGeometryMultipoint': '',
+      'esriGeometryPolyline': {color: 'blue', weight: 3 },
+      'esriGeometryPolygon': {color: 'blue', weight: 2 }
+  },
 
   load: function() {
     this.options.bbox = L.bboxToBounds(this.data.mapBbox);
@@ -8,50 +17,85 @@ GeoBlacklight.Viewer.Esrimapservice = GeoBlacklight.Viewer.Map.extend({
     this.map.addLayer(this.selectBasemap());
     this.map.addLayer(this.overlay);
     if (this.data.available) {
-      this.addPreviewLayer();
-      this.addOpacityControl();
+      this.getEsriLayer();
     } else {
       this.addBoundsOverlay(this.options.bbox);
     }
   },
 
+  getEsriLayer: function() {
+    var _this = this;
+    var url;
+
+    // remove any trailing slash from endpoint url
+    _this.data.url = _this.data.url.replace(/\/$/, '');
+
+    // get layer info as json
+    url = _this.data.url + '?f=json';
+    $.getJSON(url, function(data) {
+      _this.layerInfo = data;
+
+      // add layer to map
+      if (_this.addPreviewLayer()) {
+
+        // add control if layer is added
+        _this.addOpacityControl();
+      }
+    });
+  },
+
   addPreviewLayer: function() {
 
-    // set options
+    // set esri leaflet options
     var options = { opacity: 1 };
 
-    // remove any trailing slash and split endpoint url
-    var pathArray = this.data.url.replace(/\/$/, "").split('/');
+    // split endpoint url
+    var pathArray = this.data.url.split('/');
     var lastSegment = pathArray[pathArray.length - 1];
-    var EsrimapserviceLayer;
+    var esriMapServiceLayer;
 
-    // if last segment is an integer, treat as feature layer
-    if (Number(lastSegment) === parseInt(lastSegment, 10)) {
-
-      /**
-        * TODO: update default styles to be more pleasing
-        *       - will probably need to get layer info via ajax call (?f=json)
-        *       - parse geometryType and set styles accordingly
-      */
-      EsrimapserviceLayer = L.esri.featureLayer(this.data.url, options);
-
-      // feature layers can have inspection
-      this.setupInspection();
+    // parse url
+    if (lastSegment === 'ImageServer') {
+      esriMapServiceLayer = L.esri.imageMapLayer(this.data.url, options);
     } else if (lastSegment === 'MapServer') {
 
-      /**
-        * FIXME: check for correct spatial reference
-        * TODO:  perhaps allow non-mercator projections
-        *         - use Proj4Leaflet
-        *         - get layer info via ajax call (?f=json)
-      */ 
-      EsrimapserviceLayer = L.esri.tiledMapLayer(this.data.url, options);
-    } else if (lastSegment === 'ImageServer') {
-      EsrimapserviceLayer = L.esri.imageMapLayer(this.data.url, options);
+      // check for correct spatial reference
+      if (this.layerInfo.spatialReference.wkid === 102100) {
+
+        /**
+          * TODO:  perhaps allow non-mercator projections and custom scales
+          *        - use Proj4Leaflet
+        */
+        esriMapServiceLayer = L.esri.tiledMapLayer(this.data.url, options);
+      }
+
+    // if last path segment is an integer
+    } else if (Number(lastSegment) === parseInt(lastSegment, 10)) {
+
+      //  test if layer is feature layer
+      if (this.layerInfo.type === 'Feature Layer') {
+
+        // set default style
+        options.style = this.getFeatureStyle();
+        esriMapServiceLayer = L.esri.featureLayer(this.data.url, options);
+
+        // feature layers can have inspection
+        this.setupInspection();
+      }
     }
 
     // if it exists, add layer to map
-    if (EsrimapserviceLayer) { this.overlay.addLayer(EsrimapserviceLayer); }
+    if (esriMapServiceLayer) {
+      this.overlay.addLayer(esriMapServiceLayer);
+      return true;
+    }
+  },
+
+  getFeatureStyle: function() {
+    var _this = this;
+
+    // lookup based on layer geometry type and return style hash
+    return function(feature) { return _this.defaultStyles[_this.layerInfo.geometryType]; };
   },
 
   addOpacityControl: function() {
