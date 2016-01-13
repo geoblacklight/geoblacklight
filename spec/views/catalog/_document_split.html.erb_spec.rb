08b1fd7a54958ca0fa8ce8cf88797c873723b064
Eliot Jordan <eliotj@princeton.edu>
2016-01-13 17:38:14 -0500

    format TODO comment properly

--- spec/views/catalog/_document_split.html.erb_spec.rb
+++ spec/views/catalog/_document_split.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe 'catalog/_document_split.html.erb' do
-  # TODO js plugin refactor really needs to be redone so that the views can be testable
+  # TODO: js plugin refactor really needs to be redone so that the views can be testable
 end
