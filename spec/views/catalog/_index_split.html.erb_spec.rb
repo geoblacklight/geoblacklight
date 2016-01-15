c9f06e2d7dcc3b5c48e088518594872e995a7e16
Eliot Jordan <eliotj@princeton.edu>
2016-01-15 13:06:23 -0500

    disable DescribeClass rule in config file

--- spec/views/catalog/_index_split.html.erb_spec.rb
+++ spec/views/catalog/_index_split.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe 'catalog/_document_split.html.erb' do # rubocop:disable RSpec/DescribeClass
+describe 'catalog/_document_split.html.erb' do
   # TODO: js plugin refactor really needs to be redone so that the views can be testable
 end
