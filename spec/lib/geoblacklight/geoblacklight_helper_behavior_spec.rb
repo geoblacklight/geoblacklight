7902c2eb4dfe8dbed7fe51178944199d64cee18c
Jack Reed <phillipjreed@gmail.com>
2016-03-24 10:09:32 -0700

    Make GeoBlacklight::Presenter helper method more generalizable with method message pass

--- spec/lib/geoblacklight/geoblacklight_helper_behavior_spec.rb
+++ spec/lib/geoblacklight/geoblacklight_helper_behavior_spec.rb
@@ -5,11 +5,20 @@ describe Geoblacklight::GeoblacklightHelperBehavior do
     Class.new.extend(described_class)
   end
 
-  describe '#wxs_identifier' do
-    it 'calls defined presenter class' do
+  describe '#geoblacklight_present' do
+    before do
       expect(dummy_class).to receive(:presenter)
-        .and_return(double(wxs_identifier: 'druid:abc123'))
-      expect(dummy_class.wxs_identifier).to eq 'druid:abc123'
+        .and_return(double(fake_name: 'druid:abc123'))
+    end
+    context 'as a Symbol' do
+      it 'calls defined presenter class' do
+        expect(dummy_class.geoblacklight_present(:fake_name)).to eq 'druid:abc123'
+      end
+    end
+    context 'as a String' do
+      it 'calls defined presenter class' do
+        expect(dummy_class.geoblacklight_present('fake_name')).to eq 'druid:abc123'
+      end
     end
   end
   describe '#presenter_class' do
