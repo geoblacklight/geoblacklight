c658b3afadae5fe47504c63f9afdf99bd6096bcc
Jack Reed <phillipjreed@gmail.com>
2016-07-26 11:22:52 -0700

    bump to v1.0.0.pre1

--- app/assets/javascripts/geoblacklight/geoblacklight.js
+++ app/assets/javascripts/geoblacklight/geoblacklight.js
@@ -20,7 +20,7 @@
 
   var GeoBlacklight = L.Class.extend({
     statics: {
-      __version__: '0.0.1',
+      __version__: '1.0.0.pre1',
 
       debounce: function(fn, delay) {
         var timeout = null;
--- lib/geoblacklight/version.rb
+++ lib/geoblacklight/version.rb
@@ -1,3 +1,3 @@
 module Geoblacklight
-  VERSION = '0.12.1'.freeze
+  VERSION = '1.0.0.pre1'.freeze
 end
