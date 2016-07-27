a386c3352a846db4272d080d0c81776ea008a884
Eliot Jordan <eliotj@princeton.edu>
2016-07-27 18:02:58 -0500

    correct fixture name in linkfy test

--- spec/features/linkified_attribute_table_spec.rb
+++ spec/features/linkified_attribute_table_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Attribute table values', js: true do
   scenario 'are linkified' do
-    visit solr_document_path 'minnesota-772ebcaf2ec0405ea1b156b5937593e7_0'
+    visit solr_document_path 'minnesota-e2f33b52-4039-4bbb-9095-b5cdc0175943'
     # Wait until SVG elements are added
     expect(page).to have_css '.leaflet-overlay-pane svg'
     page.first('svg g path').click
