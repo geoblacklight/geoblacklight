c81a173b70e374fe2f4817e3c1c2fe8762eeca2f
Jack Reed <phillipjreed@gmail.com>
2016-08-04 14:06:11 -0400

    Bump to v1.0.3 :hammer:

--- app/assets/javascripts/geoblacklight/geoblacklight.js
+++ app/assets/javascripts/geoblacklight/geoblacklight.js
@@ -20,7 +20,7 @@
 
   var GeoBlacklight = L.Class.extend({
     statics: {
-      __version__: '1.0.2',
+      __version__: '1.0.3',
 
       debounce: function(fn, delay) {
         var timeout = null;
