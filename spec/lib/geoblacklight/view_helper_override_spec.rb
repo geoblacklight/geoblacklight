cbde6c6faa2f6fdc75f181537fabacf55ae90819
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-08-08 17:02:28 -0400

    make bounding box constraint label configurable

--- spec/lib/geoblacklight/view_helper_override_spec.rb
+++ spec/lib/geoblacklight/view_helper_override_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Geoblacklight::ViewHelperOverride do
   class GeoblacklightControllerTestClass
+    include AbstractController::Translation
     attr_accessor :params
   end
 
