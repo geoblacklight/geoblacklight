a6d53b3b803607aeff9b5ffa6997a3bae7703931
Jack Reed <phillipjreed@gmail.com>
2018-01-24 13:07:14 -0700

    restricts how the metadata modal gets styled so that global modals are not changed

--- app/assets/javascripts/geoblacklight/modules/metadata.js
+++ app/assets/javascripts/geoblacklight/modules/metadata.js
@@ -1,7 +1,15 @@
 Blacklight.onLoad(function() {
-  $('#ajax-modal').on('loaded.blacklight.ajax-modal', function() {
+  $('#ajax-modal').on('loaded.blacklight.ajax-modal', function(e) {
+    $(e.target).find('.metadata-body').each(function(_i, el) {
+      $(el).parent().parent().addClass('metadata-modal');
+    });
     $(this).find('.pill-metadata').each(function(i, element) {
       GeoBlacklight.metadataDownloadButton(element);
+    });
+  });
+  $('#ajax-modal').on('hidden.bs.modal', function(e) {
+    $(e.target).find('.metadata-body').each(function(_i, el) {
+      $(el).parent().parent().removeClass('metadata-modal');
     });
   });
 });
