ec264c31f3d7096dc45d6564e9190fdf968bdb2a
Eric Larson <ewlarson@umn.edu>
2018-07-24 10:34:02 -0500

    Remove: ControllerOverride
    BL7 introduces ActionBuilder, which dynamically adds controller methods from CatalogController config.add_show_tools_partial statements.
    
    ActionBuilder also returns an array of documents, rather than a single document object. Adjusting partials to match.

--- lib/geoblacklight.rb
+++ lib/geoblacklight.rb
@@ -5,7 +5,7 @@ module Geoblacklight
   require 'geoblacklight/bounding_box'
   require 'geoblacklight/catalog_helper_override'
   require 'geoblacklight/constants'
-  require 'geoblacklight/controller_override'
+  # require 'geoblacklight/controller_override'
   require 'geoblacklight/exceptions'
   require 'geoblacklight/geoblacklight_helper_behavior'
   require 'geoblacklight/view_helper_override'
@@ -34,7 +34,7 @@ module Geoblacklight
   require 'geoblacklight/relation/relation_response'
 
   def self.inject!
-    CatalogController.send(:include, Geoblacklight::ControllerOverride)
+    # CatalogController.send(:include, Geoblacklight::ControllerOverride)
     CatalogController.send(:include, Geoblacklight::CatalogHelperOverride)
     CatalogController.send(:include, Geoblacklight::ViewHelperOverride)
     CatalogController.send(:helper, Geoblacklight::ViewHelperOverride) unless
--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -5,7 +5,7 @@ describe CatalogController, type: :controller do
     it 'returns a document based off an id' do
       get :web_services, params: { id: 'mit-us-ma-e25zcta5dct-2000' }
       expect(response.status).to eq 200
-      expect(assigns(:document)).not_to be_nil
+      expect(assigns(:documents)).not_to be_nil
     end
   end
 
