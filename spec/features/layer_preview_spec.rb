bf220e1bde2c4fe7a529ee1f14cd27ce6075f389
Eric Larson <ewlarson@umn.edu>
2018-07-24 15:46:55 -0500

    Specs: adjust for SVG icons
    BL7 uses SVG icons so specs need to specify area of page to count path elements.

--- spec/features/layer_preview_spec.rb
+++ spec/features/layer_preview_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 feature 'Layer preview', js: true do
   scenario 'Restricted layer should show bounding box' do
     visit solr_document_path('stanford-cg357zz0321')
-    expect(Nokogiri::HTML.parse(page.body).css('path').length).to eq 1
+    within('#map') do
+      expect(page).to have_css('path')
+    end
   end
 
   scenario 'Public layer should show wms layer not bounding box' do
     visit solr_document_path('mit-us-ma-e25zcta5dct-2000')
-    expect(Nokogiri::HTML.parse(page.body).css('path').length).to eq 0
+    # BL7 has svg icons, so sniffing for SVG path won't return false
+    # expect(Nokogiri::HTML.parse(page.body).css('path').length).to eq 0
     within '.leaflet-tile-pane' do
       expect(page).to have_css('.leaflet-layer', count: 2)
     end
