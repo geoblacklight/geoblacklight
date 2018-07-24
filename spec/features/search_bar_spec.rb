5e22bb962e03b436a10471da5fa348810b6e1884
Eric Larson <ewlarson@umn.edu>
2018-07-24 15:56:25 -0500

    Specs: #search-navbar is gone; .navbar-search is here

--- spec/features/search_bar_spec.rb
+++ spec/features/search_bar_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 feature 'search bar' do
   scenario 'present on a spatial search' do
     visit search_catalog_path(bbox: '25 3 75 35')
-    expect(page).to have_css '#search-navbar'
+    expect(page).to have_css '.navbar-search'
   end
   scenario 'present on a text search' do
     visit search_catalog_path(q: 'test')
-    expect(page).to have_css '#search-navbar'
+    expect(page).to have_css '.navbar-search'
   end
 end
