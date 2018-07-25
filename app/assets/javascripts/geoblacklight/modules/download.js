e1f95516cddc787cf89057d62bc5977f16728df9
Eric Larson <ewlarson@umn.edu>
2018-07-25 10:27:03 -0500

    HGL Downloader: fix JS data attr listener, modal selectors
    BL7 uses data-blacklight-modal for JS event listener and #blacklight-modal instead of #ajax-modal

--- app/assets/javascripts/geoblacklight/modules/download.js
+++ app/assets/javascripts/geoblacklight/modules/download.js
@@ -2,7 +2,7 @@ Blacklight.onLoad(function() {
   $('[data-download-path]').each(function(i, element) {
     GeoBlacklight.downloader(element);
   });
-  $('#ajax-modal').on('loaded.blacklight.ajax-modal', function() {
+  $('#blacklight-modal').on('loaded.blacklight.blacklight-modal', function() {
     var modal = this;
     $(this).find('#hglRequest').each(function() {
       GeoBlacklight.hglDownloader(this, { modal: modal });
