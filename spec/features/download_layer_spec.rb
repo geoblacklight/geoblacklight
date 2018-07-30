ed798731d26a43070411da2f34d64b2ecb7a743e
GriffinJ <jrgriffiniii@gmail.com>
2018-07-30 18:31:16 -0400

    Adjusts the download and export panel styles for Bootstrap 4

--- spec/features/download_layer_spec.rb
+++ spec/features/download_layer_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 feature 'Download layer' do
   let(:shapefile_download) { instance_double(Geoblacklight::ShapefileDownload) }
   let(:kmz_download) { instance_double(Geoblacklight::KmzDownload) }
-  let(:hgl_download) { instance_double(Geoblacklight::HglDownload) }
 
   before do
     allow(Geoblacklight::ShapefileDownload).to receive(:new).and_return(shapefile_download)
     allow(Geoblacklight::KmzDownload).to receive(:new).and_return(kmz_download)
-    allow(Geoblacklight::HglDownload).to receive(:new).and_return(hgl_download)
   end
 
   scenario 'clicking initial shapefile download button should trigger download', js: true do
@@ -73,15 +71,20 @@ feature 'Download layer' do
     find('a[data-download-type="harvard-hgl"]', text: 'GeoTIFF').click
     expect(page).to have_css('#hglRequest')
   end
-  scenario 'submitting email form should trigger HGL request', js: true do
-    expect(hgl_download).to receive(:get).and_return('success')
-    visit solr_document_path('harvard-g7064-s2-1834-k3')
-    find('a[data-download-type="harvard-hgl"]', text: 'GeoTIFF').click
-    within '#hglRequest' do
-      fill_in('Email', with: 'foo@example.com')
-      click_button('Request')
-    end
-    using_wait_time 120 do
+  context 'with a successful request to the server' do
+    let(:hgl_download) { instance_double(Geoblacklight::HglDownload) }
+
+    scenario 'submitting email form should trigger HGL request', js: true do
+      visit solr_document_path('harvard-g7064-s2-1834-k3')
+      find('a[data-download-type="harvard-hgl"]', text: 'Original GeoTIFF').click
+
+      allow_any_instance_of(Geoblacklight::HglDownload).to receive(:new).and_return(hgl_download)
+      allow(hgl_download).to receive(:get).and_return('success')
+
+      within '#hglRequest' do
+        fill_in('Email', with: 'foo@example.com')
+        click_button('Request')
+      end
       expect(page).to have_css('.alert-success')
       expect(page).to have_content('You should receive an email when your download is ready')
     end
