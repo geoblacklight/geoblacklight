5a672d3f03adb0f3c5f1ef10a581ff57a715eb18
GriffinJ <jrgriffiniii@gmail.com>
2018-07-30 18:31:16 -0400

    Adjusts the download and export panel styles for Bootstrap 4

--- app/helpers/geoblacklight_helper.rb
+++ app/helpers/geoblacklight_helper.rb
@@ -42,7 +42,7 @@ module GeoblacklightHelper
       download_hgl_path(id: document),
       class: ['btn', 'btn-default', 'download', 'download-original'],
       data: {
-        ajax_modal: 'trigger',
+        blacklight_modal: 'trigger',
         download: 'trigger',
         download_type: 'harvard-hgl',
         download_id: document.id
