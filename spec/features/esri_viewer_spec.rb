b74325c1173dbf0846f23e85b3a5f345a8105feb
Eliot Jordan <eliotj@princeton.edu>
2018-08-03 11:08:47 -0500

    Fix tests broken by updating fixtures

--- spec/features/esri_viewer_spec.rb
+++ spec/features/esri_viewer_spec.rb
@@ -7,18 +7,18 @@ feature 'feature_layer reference', js: true do
     expect(page).to have_css 'img.leaflet-image-layer', visible: true
   end
   scenario 'displays dynamic layer (all layers)' do
-    visit solr_document_path 'minnesota-f14ff4-1359-4beb-b931-5cb41d20ab90'
+    visit solr_document_path '90f14ff4-1359-4beb-b931-5cb41d20ab90'
     expect(page).to have_css '.leaflet-control-zoom', visible: true
     expect(page).to have_css 'img.leaflet-image-layer', visible: true
   end
   scenario 'displays dynamic layer (single layer)' do
-    visit solr_document_path 'Minnesota-urn-0f7ae38b-4bf2-4e03-a32b-e87f245ccb03'
+    visit solr_document_path '4669301e-b4b2-4c8b-bf40-01b968a2865b'
     expect(page).to have_css '.leaflet-control-zoom', visible: true
     expect(page).to have_css 'img.leaflet-image-layer', visible: true
   end
   scenario 'displays feature layer' do
     pending 'cannot currently test for svg feature'
-    visit solr_document_path 'minnesota-e2f33b52-4039-4bbb-9095-b5cdc0175943'
+    visit solr_document_path 'f406332e63eb4478a9560ad86ae90327_18'
     expect(page).to have_css '.leaflet-control-zoom', visible: true
     expect(Nokogiri::HTML.parse(page.body).css('g').length).to eq 23
     fail
