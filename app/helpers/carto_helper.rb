bf3415ee06650f9c6f592d83b960a602b5ca0d23
Eliot Jordan <eliotj@princeton.edu>
2019-01-22 14:13:04 -0600

    Removed CartDB deprecation
    Co-authored-by: Christina Chortaria <actspatial@gmail.com>

--- app/helpers/carto_helper.rb
+++ app/helpers/carto_helper.rb
@@ -1,6 +1,6 @@
 module CartoHelper
   ##
-  # Creates a Carto OneClick link link, using the configuration link
+  # Creates a Carto OneClick link, using the configuration link
   # @param [String] file_link
   # @return [String]
   def carto_link(file_link)
@@ -24,13 +24,6 @@ module CartoHelper
   ##
   # Method used to access setting and provide deprecation warnings to migrate
   def carto_oneclick_host
-    if Settings.CARTODB_ONECLICK_LINK.present?
-      Deprecation.warn(
-        GeoblacklightHelper,
-        'Settings.CARTODB_ONECLICK_LINK is deprecated and will be removed in ' \
-        'Geoblacklight 2.0.0, use Settings.CARTO_ONECLICK_LINK instead'
-      )
-    end
-    Settings.CARTO_ONECLICK_LINK || Settings.CARTODB_ONECLICK_LINK
+    Settings.CARTO_ONECLICK_LINK
   end
 end
