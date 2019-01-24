03f2eb5f27ff5dc0f1e713c5e66192ac6df20695
James R. Griffin III <jrgriffiniii@gmail.com>
2019-01-24 16:01:38 -0500

    Ensuring that the SMS modal is rendered by default (#740)

--- lib/generators/geoblacklight/templates/catalog_controller.rb
+++ lib/generators/geoblacklight/templates/catalog_controller.rb
@@ -239,6 +239,7 @@ class CatalogController < ApplicationController
     config.add_results_collection_tool(:per_page_widget)
     config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
 
     # Custom tools for GeoBlacklight
     config.add_show_tools_partial :web_services, if: proc { |_context, _config, options| options[:document] && (Settings.WEBSERVICES_SHOWN & options[:document].references.refs.map(&:type).map(&:to_s)).any? }
