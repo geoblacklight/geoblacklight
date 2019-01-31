d9b16a2070ec75cb19fb5bc16f5e43f463877a28
Eliot Jordan <eliotj@princeton.edu>
2019-01-31 14:07:32 -0600

    Add timeout values to selenium http client

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,10 +31,13 @@ Capybara.register_driver(:headless_chrome) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w(headless disable-gpu window-size=1280,1024) }
   )
-
+  http_client = Selenium::WebDriver::Remote::Http::Default.new
+  http_client.read_timeout = 120
+  http_client.open_timeout = 120
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
-                                 desired_capabilities: capabilities)
+                                 desired_capabilities: capabilities,
+                                 http_client: http_client)
 end
 
 Capybara.javascript_driver = :headless_chrome
