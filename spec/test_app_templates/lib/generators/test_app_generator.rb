0e7f357efb5613596810fa836aed0c416f7c445e
Eliot Jordan <eliot.jordan@gmail.com>
2019-02-05 17:19:19 -0800

    Add fix rails/sqlite3 issue (#778)
    * Add fix rails/sqlite3 issue
    
    * Update chrome in Circle CI config

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -3,6 +3,14 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path('../../../../spec/test_app_templates', __FILE__)
 
+  def fix_sqlite3_version_requirement
+    return unless Gem.loaded_specs['rails'].version.to_s <= '5.2.2'
+
+    # Hack for https://github.com/rails/rails/issues/35153
+    # Adapted from https://github.com/projectblacklight/blacklight/pull/2065
+    gsub_file('Gemfile', /^gem 'sqlite3'$/, 'gem "sqlite3", "~> 1.3.6"')
+  end
+
   def add_gems
     gem 'blacklight'
     gem 'teaspoon'
