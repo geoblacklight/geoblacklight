2b3430d0d3a42032bc1cff56d9ee28561aa38dc9
Jack Reed <phillipjreed@gmail.com>
2019-05-10 17:33:13 -0700

    Fixes #785 by providing a #pagination wrapping div that can be replaced by our js

---
+++ spec/views/catalog/_results_pagination.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'catalog/_results_pagination.html.erb', type: :view do
+  it 'will have a #pagination wrapping div' do
+    allow(view).to receive_messages(show_pagination?: false)
+    render
+    expect(rendered).to have_css '#pagination'
+  end
+end
