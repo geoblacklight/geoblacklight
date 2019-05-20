9a8b89dd9a1dfe30d0f56af88ee38a10aa6bc05b
Jack Reed <phillipjreed@gmail.com>
2019-05-20 12:00:04 -0600

    Add js true for needed browse spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -6,7 +6,7 @@ feature 'Search' do
     expect(page).to have_css '.document', count: 1
   end
 
-  scenario 'When searching child records from a parent record, supressed records are not hidden' do
+  scenario 'When searching child records from a parent record, supressed records are not hidden', js: true do
     visit '/catalog/princeton-1r66j405w'
     click_link("Browse all 4 records...")
     expect(page).to have_css '.document', count: 4
