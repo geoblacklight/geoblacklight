66376c527cd81a1aec2abf589d5751783953069a
Jack Reed <phillipjreed@gmail.com>
2019-05-20 12:42:08 -0600

    Appease the rubocop

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,7 +8,7 @@ feature 'Search' do
 
   scenario 'When searching child records from a parent record, supressed records are not hidden', js: true do
     visit '/catalog/princeton-1r66j405w'
-    click_link("Browse all 4 records...")
+    click_link 'Browse all 4 records...'
     expect(page).to have_css '.document', count: 4
   end
 end
