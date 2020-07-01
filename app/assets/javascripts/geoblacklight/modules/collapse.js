68340ce26fe422dd74248fcdeab7eb352ce7496c
Chris Beer <cabeer@stanford.edu>
2020-07-01 09:37:54 -0700

    Redo the search results display to be accessible
    - Use bootstrap collapse to handle collapsing
    - Make the collapse toggle a button
    - Use flexbox for truncation

--- app/assets/javascripts/geoblacklight/modules/collapse.js
+++
@@ -1,7 +0,0 @@
-Blacklight.onLoad(function() {
-  $('#content')
-    .on('click', '#documents [data-layer-id]', function() {
-      $(this).find('.collapse').collapse('toggle');
-      $(this).find('.caret-toggle').toggleClass('open');
-    });
-});
