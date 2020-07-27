9059681af79c05f1882e54fb4be4cc3be056784f
Jessie Keck <jessie.keck@gmail.com>
2020-07-27 15:02:03 -0700

    Fix show_tools partial spec for rails 5/6 compatibility

--- spec/views/catalog/_show_tools.html.erb_spec.rb
+++ spec/views/catalog/_show_tools.html.erb_spec.rb
@@ -6,11 +6,8 @@ describe 'catalog/_show_tools.html.erb', type: :view do
   let(:context) { Blacklight::Configuration::Context.new(controller) }
 
   before do
-    assign :response, instance_double(Blacklight::Solr::Response, params: {})
     assign :document, document
     allow(view).to receive(:blacklight_config).and_return blacklight_config
-    allow(view).to receive(:has_user_authentication_provider?).and_return false
-
     allow(view).to receive(:blacklight_configuration_context).and_return context
   end
 
@@ -18,7 +15,7 @@ describe 'catalog/_show_tools.html.erb', type: :view do
     let(:document_actions) { blacklight_config.show.document_actions }
 
     it 'renders a document action' do
-      allow(view).to receive(:some_action_solr_document_path).with(document).and_return 'x'
+      allow(view).to receive(:some_action_solr_document_path).with(document, any_args).and_return 'x'
       document_actions[:some_action] = Blacklight::Configuration::ToolConfig.new partial: 'document_action'
       render partial: 'catalog/show_tools'
       expect(rendered).to have_link 'Some action', href: 'x'
