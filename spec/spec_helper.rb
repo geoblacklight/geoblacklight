826b2f38d07716d1828b4619afb1bdc481d517e8
Eliot Jordan <eliotj@princeton.edu>
2020-08-07 14:32:56 -0500

    Rubocop: Fix RSpec/HookArgument

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,7 +59,7 @@ FactoryBot.find_definitions
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
-  config.before :each do
+  config.before do
     DatabaseCleaner.strategy = if Capybara.current_driver == :rack_test
                                  :transaction
                                else
