976516e53c58935b7d75b649401f92a2a84df81b
Eric Larson <ewlarson@gmail.com>
2021-02-23 14:40:41 -0600

    Remove: duplicate Settings.FIELD.LAYER_ID
    Use original WXS_IDENTIFIER key/value instead.

--- spec/config/initializers/rails_config_spec.rb
+++ spec/config/initializers/rails_config_spec.rb
@@ -5,7 +5,6 @@ describe 'Config' do
   it 'Loads new v3.3 Settings.FIELDS defaults' do
     expect(Settings.FIELDS.IDENTIFIER).to eq 'dc_identifier_s'
     expect(Settings.FIELDS.LANGUAGE).to eq 'dc_language_s'
-    expect(Settings.FIELDS.LAYER_ID).to eq 'layer_id_s'
     expect(Settings.FIELDS.LAYER_MODIFIED).to eq 'layer_modified_dt'
     expect(Settings.FIELDS.SOURCE).to eq 'dc_source_sm'
     expect(Settings.FIELDS.SUPPRESSED).to eq 'suppressed_b'
