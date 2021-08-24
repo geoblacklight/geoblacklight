8dfe30d544fd5c970e92897980abd8806c176e6e
Eric Larson <ewlarson@gmail.com>
2021-08-24 10:29:18 -0500

    Config: remove new v3.3 defaults, fix failing config spec

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -2,11 +2,3 @@
 Config.setup do |config|
   config.const_name = 'Settings'
 end
-
-# GeoBlacklight v3.3 - New Settings.FIELDS values
-Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_3_3.yml', __dir__))
-Settings.reload!
-
-# GeoBlacklight v3.4 - New Settings.HOMEPAGE_MAP_GEOM option
-Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_3_4.yml', __dir__))
-Settings.reload!
