c39c0a868777b9fd25f2fe5903a3ea94302339be
Eric Larson <ewlarson@gmail.com>
2021-08-26 16:26:22 -0500

    Aardvark Relationships: finishes implementation
    RelationResponse now only uses #method_missing to retrieve relations from Solr.
    
    Relation#index(html and json) use Settings.RELATIONSHIPS_SHOWN to loop relation values for display
    
    Relation specs updated per code changes.

--- spec/config/initializers/rails_config_spec.rb
+++ spec/config/initializers/rails_config_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 
 describe 'Config' do
   it 'Loads new v3.3 Settings.FIELDS defaults' do
-    expect(Settings.FIELDS.IDENTIFIER).to eq 'dct_identifier_sm'
-    expect(Settings.FIELDS.LANGUAGE).to eq 'dct_language_sm'
-    expect(Settings.FIELDS.LAYER_MODIFIED).to eq 'gbl_mdModified_dt'
-    expect(Settings.FIELDS.SOURCE).to eq 'dct_source_sm'
-    expect(Settings.FIELDS.SUPPRESSED).to eq 'gbl_suppressed_b'
+    expect(Settings.FIELDS.IDENTIFIER).to eq 'dc_identifier_s'
+    expect(Settings.FIELDS.LANGUAGE).to eq 'dc_language_s'
+    expect(Settings.FIELDS.LAYER_MODIFIED).to eq 'layer_modified_dt'
+    expect(Settings.FIELDS.SOURCE).to eq 'dc_source_sm'
+    expect(Settings.FIELDS.SUPPRESSED).to eq 'suppressed_b'
     expect(Settings.FIELDS.TYPE).to eq 'dc_type_s'
-    expect(Settings.FIELDS.UNIQUE_KEY).to eq 'id'
+    expect(Settings.FIELDS.UNIQUE_KEY).to eq 'layer_slug_s'
   end
 
   it 'Loads new v3.4 Settings.FIELDS defaults' do
