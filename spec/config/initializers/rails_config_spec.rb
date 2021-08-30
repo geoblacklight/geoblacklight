db17a76c6e72b8612bb1118b355d750dd0c3d3e6
Eric Larson <ewlarson@gmail.com>
2021-08-30 10:21:50 -0500

    Update rails_config_spec.rb
    Remove the v3.3 default config settings.

--- spec/config/initializers/rails_config_spec.rb
+++ spec/config/initializers/rails_config_spec.rb
@@ -2,16 +2,6 @@
 require 'spec_helper'
 
 describe 'Config' do
-  it 'Loads new v3.3 Settings.FIELDS defaults' do
-    expect(Settings.FIELDS.IDENTIFIER).to eq 'dct_identifier_sm'
-    expect(Settings.FIELDS.LANGUAGE).to eq 'dct_language_sm'
-    expect(Settings.FIELDS.LAYER_MODIFIED).to eq 'gbl_mdModified_dt'
-    expect(Settings.FIELDS.SOURCE).to eq 'dct_source_sm'
-    expect(Settings.FIELDS.SUPPRESSED).to eq 'gbl_suppressed_b'
-    expect(Settings.FIELDS.TYPE).to eq 'dc_type_s'
-    expect(Settings.FIELDS.UNIQUE_KEY).to eq 'id'
-  end
-
   it 'Loads new Aardvark relationships' do
     expect(Settings).to respond_to('RELATIONSHIPS_SHOWN')
     [:field, :query_type, :icon, :label].each do |method|
