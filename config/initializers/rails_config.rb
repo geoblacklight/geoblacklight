ad09919261baa6d981fa53333ecf685fca779455
Eric Larson <ewlarson@gmail.com>
2021-09-03 15:24:55 -0500

    Updated to use Settings.HOMEPAGE_MAP_GEOM
    Is null by default. Includes example for scoping the map via a GeoJSON object.

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -6,3 +6,7 @@ end
 # GeoBlacklight v3.3 - New Settings.FIELDS values
 Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_3_3.yml', __dir__))
 Settings.reload!
+
+# GeoBlacklight v3.4 - New Settings.FIELDS values
+Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_3_4.yml', __dir__))
+Settings.reload!
