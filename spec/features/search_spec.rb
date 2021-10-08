6b08a04212cd1000b03d15b2f196279138cbe7ee
Eric Larson <ewlarson@gmail.com>
2021-10-08 14:35:02 -0500

    Bug fix for Aardvark relationship browse links
    The relationship "Browse" link should query Solr for the related field from the relationship: pcdm_memberOf_sm, dct_isPartOf_sm, dct_relation_sm, dct_source_sm, etc.
    
    This fix adds a relationship selector to the relationship div.card, ex. "relationship-source_descendants". And it scopes the Solr query to the proper relationship field.
    
    Fixes #1099

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -9,6 +9,11 @@ feature 'Search' do
 
   scenario 'When searching child records from a parent record, supressed records are not hidden', js: true do
     visit '/catalog/princeton-1r66j405w'
+
+    within('.card.relations.relationship-source_descendants') do
+      expect(page).to have_link(href: /f%5Bdct_source_sm%5D/)
+    end
+
     click_link 'Browse all 4 records...'
     expect(page).to have_css '.document', count: 4
   end
