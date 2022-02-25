69be3db5be445396e46f4dea0d5fa710e3543308
Eric Larson <ewlarson@gmail.com>
2022-02-25 08:36:18 -0600

    Specs: Bump dt and dd counts for new default metadata configuration

--- spec/features/show_page_metadata_spec.rb
+++ spec/features/show_page_metadata_spec.rb
@@ -5,8 +5,8 @@ feature 'Metadata display on show page' do
   scenario 'with default CatalogController specified fields' do
     visit solr_document_path 'stanford-dp018hs9766'
     within '.document-metadata' do
-      expect(page).to have_css 'dt', count: 8
-      expect(page).to have_css 'dd', count: 8
+      expect(page).to have_css 'dt', count: 16
+      expect(page).to have_css 'dd', count: 16
       expect(page).to have_css 'div.truncate-abstract', count: 1
     end
   end
