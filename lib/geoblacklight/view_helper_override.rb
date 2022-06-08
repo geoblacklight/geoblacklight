f1d24e4224a3653336a18a1b3fa7e8d0bced1743
Eric Larson <ewlarson@gmail.com>
2022-06-08 17:29:19 -0500

    Update view_helper_override.rb
    Add missing method.

--- lib/geoblacklight/view_helper_override.rb
+++ lib/geoblacklight/view_helper_override.rb
@@ -2,5 +2,14 @@
 module Geoblacklight
   # Override Blacklight helpers to inject behavior
   module ViewHelperOverride
+    def render_search_to_s(params)
+      super + render_search_to_s_bbox(params)
+    end
+
+    def render_search_to_s_bbox(params)
+      return ''.html_safe if params['bbox'].blank?
+
+      render_search_to_s_element(t('geoblacklight.bbox_label'), render_filter_value(params['bbox']))
+    end
   end
 end
