a4fea2ec258abf417b6c7351c2e3d93036e8a9fe
Chris Beer <chris@cbeer.info>
2022-06-08 11:23:27 -0700

    Remove template suffixes for testing

--- spec/views/catalog/_results_pagination.html.erb_spec.rb
+++ spec/views/catalog/_results_pagination.html.erb_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-describe 'catalog/_results_pagination.html.erb', type: :view do
+describe 'catalog/_results_pagination', type: :view do
   it 'will have a #pagination wrapping div' do
     allow(view).to receive_messages(show_pagination?: false)
     render
