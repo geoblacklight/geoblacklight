29b50255db6dbf7f9628c5adf49475eec01cb2d6
Eric Larson <ewlarson@gmail.com>
2022-09-16 10:26:56 -0500

    Update template.rb
    Set GBL to latest v4.0 release.

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 gem 'blacklight', '~> 7.0'
-gem 'geoblacklight', '~> 4.0.0.pre.rc3'
+gem 'geoblacklight', '~> 4.0'
 gem 'sprockets', '< 4.0' # Use sprockets less than 4.0, let webpacker users set this up themselves
 
 # Hack for https://github.com/rails/rails/issues/35153
