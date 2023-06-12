ce24fc7d1eabb60462c2eeb4860e4414800554c3
Eric Larson <ewlarson@gmail.com>
2023-06-12 11:28:20 -0500

    Update rails_config.rb
    StandardRB fixes

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -5,5 +5,5 @@ Config.setup do |config|
 end
 
 # GeoBlacklight v4.1 - New Settings values
-Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_4_1.yml', __dir__))
+Settings.prepend_source!(File.expand_path("new_gbl_settings_defaults_4_1.yml", __dir__))
 Settings.reload!
