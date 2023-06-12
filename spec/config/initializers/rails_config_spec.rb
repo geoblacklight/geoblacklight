60a64086a524fddc6cd8063dba3fadb7b885065c
Eric Larson <ewlarson@gmail.com>
2023-06-12 11:24:46 -0500

    Config: check for new v4.1 settings.yml values

--- spec/config/initializers/rails_config_spec.rb
+++ spec/config/initializers/rails_config_spec.rb
@@ -3,14 +3,14 @@
 require "spec_helper"
 
 describe "Config" do
-  it "Loads new Aardvark relationships" do
+  it "Loads new v4.1 Settings defaults" do
+    expect(Settings.FIELDS.DISPLAY_NOTE).to respond_to(:field)
+    expect(Settings.DISPLAY_NOTES_SHOWN).to respond_to(:danger)
+    expect(Settings.SIDEBAR_STATIC_MAP).to respond_to(:iiif)
+
     expect(Settings).to respond_to("RELATIONSHIPS_SHOWN")
     [:field, :query_type, :icon, :label].each do |method|
-      expect(Settings.RELATIONSHIPS_SHOWN.MEMBER_OF).to respond_to(method)
+      expect(Settings.RELATIONSHIPS_SHOWN.MEMBER_OF_ANCESTORS).to respond_to(method)
     end
-  end
-
-  it "Loads new v3.4 Settings.FIELDS defaults" do
-    expect(Settings.HOMEPAGE_MAP_GEOM).to be_nil
   end
 end
