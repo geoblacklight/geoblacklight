03efb3d8fd96de66b757798bea90bd6a352d01c3
Eric Larson <ewlarson@gmail.com>
2023-06-12 11:24:46 -0500

    Config: check for new v4.1 settings.yml values

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -3,3 +3,7 @@
 Config.setup do |config|
   config.const_name = "Settings"
 end
+
+# GeoBlacklight v4.1 - New Settings values
+Settings.prepend_source!(File.expand_path('new_gbl_settings_defaults_4_1.yml', __dir__))
+Settings.reload!
