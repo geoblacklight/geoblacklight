9232cf12d3422279f6eab92e8b0a6a0488770c5a
Michael Cain <mmc469@nyu.edi>
2023-07-17 17:23:05 -0500

    add view component to _home_text view

--- spec/components/geoblacklight/homepage_map_component_spec.rb
+++ spec/components/geoblacklight/homepage_map_component_spec.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Geoblacklight::HomepageMapComponent, type: :component do
   subject(:rendered) do
-    render_inline_to_capybara_node(described_class.new)
+    render_inline_to_capybara_node(described_class.new(basemap: 'positron',
+                                                       catalog_path: '/catalog',
+                                                       leaflet_options: Settings.LEAFLET.to_h,
+                                                       map_geometry: Settings.HOMEPAGE_MAP_GEOM ))
   end
 
-  it 'renders the map' do
-    expect(rendered).to have_selector 'div#map'
-    expect(rendered).to have_selector 'h3'
+  it "renders the map" do
+    expect(rendered).to have_selector "div#map"
+    expect(rendered).to have_selector "h3"
   end
 end
