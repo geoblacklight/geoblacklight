22ab7b098e7d5134d4d4bb25ed04022d195b19a4
Michael Cain <mmc469@nyu.edi>
2023-07-18 08:13:54 -0500

    rubocop linting

--- spec/components/geoblacklight/homepage_map_component_spec.rb
+++ spec/components/geoblacklight/homepage_map_component_spec.rb
@@ -4,10 +4,10 @@ require "spec_helper"
 
 RSpec.describe Geoblacklight::HomepageMapComponent, type: :component do
   subject(:rendered) do
-    render_inline_to_capybara_node(described_class.new(basemap: 'positron',
-                                                       catalog_path: '/catalog',
-                                                       leaflet_options: Settings.LEAFLET.to_h,
-                                                       map_geometry: Settings.HOMEPAGE_MAP_GEOM ))
+    render_inline_to_capybara_node(described_class.new(basemap: "positron",
+      catalog_path: "/catalog",
+      leaflet_options: Settings.LEAFLET.to_h,
+      map_geometry: Settings.HOMEPAGE_MAP_GEOM))
   end
 
   it "renders the map" do
