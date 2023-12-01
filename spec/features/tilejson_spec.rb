85fb89a83f771245bb9d80c033248a88f999039e
Eliot Jordan <eliotj@princeton.edu>
2023-12-01 13:26:26 -0600

    Don't test live response in TileJSON and WMTS feature specs

--- spec/features/tilejson_spec.rb
+++ spec/features/tilejson_spec.rb
@@ -13,11 +13,13 @@ feature "tilejson layer" do
 
   scenario "displays tilejson layer", js: true do
     # Mock tilejson manifest url
-    stub_request(:get, "https://map-tiles-staging.princeton.edu/mosaicjson/tilejson.json?id=2a91d82c541c426cb787cc62afe8f248")
+    stub_request(:get, "https://map-tiles-staging.princeton.edu/2a91d82c541c426cb787cc62afe8f248/mosaicjson/tilejson.json")
       .to_return(status: 200, body: read_fixture("manifests/tilejson.json"))
 
     visit solr_document_path("princeton-fk4544658v-tilejson")
+
     expect(page).to have_css ".leaflet-control-zoom", visible: :visible
-    expect(page).to have_css "img[src*='https://map-tiles-staging.princeton.edu/mosaicjson/tiles/WebMercatorQuad']"
+    expect(page).to have_css "div[data-protocol='Tilejson']"
+    expect(page).to have_css "div[data-url='https://map-tiles-staging.princeton.edu/2a91d82c541c426cb787cc62afe8f248/mosaicjson/tilejson.json']"
   end
 end
