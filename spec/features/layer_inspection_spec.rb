80484cb9e4dedb2ae3909eb25042ae8c42f5a9b4
Eliot Jordan <eliotj@princeton.edu>
2023-12-01 13:26:54 -0600

    Use NYU WMS service to test feature inspection

--- spec/features/layer_inspection_spec.rb
+++ spec/features/layer_inspection_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 feature "Layer inspection", js: true do
   scenario "clicking map should trigger inspection" do
-    visit solr_document_path("mit-f6rqs4ucovjk2")
+    visit solr_document_path("nyu-2451-34564")
     expect(page).to have_css("th", text: "Attribute")
     find("#map").click
     expect(page).not_to have_css("td.default-text")
