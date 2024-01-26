5b94c76cf83bef6efa7dba6f41f7906c60b17008
Eliot Jordan <eliotj@princeton.edu>
2024-01-26 13:54:22 -0600

    Fix linting errors

--- lib/geoblacklight/faraday_middleware/follow_redirects.rb
+++ lib/geoblacklight/faraday_middleware/follow_redirects.rb
@@ -18,7 +18,7 @@ module Geoblacklight
       attr_reader :response
 
       def initialize(response)
-        super "too many redirects; last one to: #{response["location"]}"
+        super("too many redirects; last one to: #{response["location"]}")
         @response = response
       end
     end
