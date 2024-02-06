708024e40e5b8ff5317ad6776568a6f1f74d119b
Eliot Jordan <eliotj@princeton.edu>
2024-02-06 14:04:06 -0600

    Symlink fixture directories

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -30,10 +30,13 @@ class TestAppGenerator < Rails::Generators::Base
     generate "geoblacklight:install", "-f"
   end
 
+  # Symlink fixture document directories so the test app doesn't have to be
+  # regenerated when they are changed or updated.
   def fixtures
-    FileUtils.mkdir_p "spec/fixtures/solr_documents"
-    directory "solr_documents", "spec/fixtures/solr_documents"
-    FileUtils.mkdir_p "spec/fixtures/metadata"
-    directory "metadata", "spec/fixtures/metadata"
+    solr_docs_path = Rails.root.join("..", "spec", "fixtures", "solr_documents")
+    metadata_path = Rails.root.join("..", "spec", "fixtures", "metadata")
+    FileUtils.mkdir_p "spec/fixtures"
+    FileUtils.symlink solr_docs_path, "spec/fixtures/solr_documents"
+    FileUtils.symlink metadata_path, "spec/fixtures/metadata"
   end
 end
