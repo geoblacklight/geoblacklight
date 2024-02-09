8ff201c94d40cd6e847200316ca67f057d5305fc
Eliot Jordan <eliotj@princeton.edu>
2024-02-09 15:10:51 -0600

    Updates to test app generation (#1347)
    * Symlink fixture directories
    
    * Remove unneeded sqlite fix

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -5,14 +5,6 @@ require "rails/generators"
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../spec/test_app_templates", __FILE__)
 
-  def fix_sqlite3_version_requirement
-    return unless Gem.loaded_specs["rails"].version.to_s <= "5.2.2"
-
-    # Hack for https://github.com/rails/rails/issues/35153
-    # Adapted from https://github.com/projectblacklight/blacklight/pull/2065
-    gsub_file("Gemfile", /^gem 'sqlite3'$/, 'gem "sqlite3", "~> 1.3.6"')
-  end
-
   def add_gems
     gem "blacklight"
     Bundler.with_clean_env do
@@ -30,10 +22,13 @@ class TestAppGenerator < Rails::Generators::Base
     generate "geoblacklight:install", "-f"
   end
 
+  # Symlink fixture document directories so the test app doesn't have to be
+  # regenerated when they are changed or updated.
   def fixtures
-    FileUtils.mkdir_p "spec/fixtures/solr_documents"
-    directory "solr_documents", "spec/fixtures/solr_documents"
-    FileUtils.mkdir_p "spec/fixtures/metadata"
-    directory "metadata", "spec/fixtures/metadata"
+    solr_docs_path = Rails.root.join("..", "spec", "fixtures", "solr_documents")
+    metadata_path = Rails.root.join("..", "spec", "fixtures", "metadata")
+    FileUtils.mkdir_p "spec/fixtures"
+    FileUtils.symlink solr_docs_path, "spec/fixtures/solr_documents"
+    FileUtils.symlink metadata_path, "spec/fixtures/metadata"
   end
 end
