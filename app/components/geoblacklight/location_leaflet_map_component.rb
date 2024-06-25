ecda237579ac501f2589b3ed8204bcbb6aed09cc
dnoneill <noneill@stanford.edu>
2024-06-25 17:27:28 -0400

    move document_split map to component, make map sticky

--- app/components/geoblacklight/location_leaflet_map_component.rb
+++ app/components/geoblacklight/location_leaflet_map_component.rb
@@ -2,10 +2,11 @@
 
 module Geoblacklight
   class LocationLeafletMapComponent < ViewComponent::Base
-    def initialize(id: "map", label: nil, data_map: "", map_geometry: nil)
+    def initialize(id: "map", label: nil, data_map: "", map_geometry: nil, classes: "")
       @id = id
       @label = label
       @data_map = data_map
+      @classes = classes
       @map_geometry = map_geometry || Settings.HOMEPAGE_MAP_GEOM
       super
     end
