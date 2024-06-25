8cca8161e471c5aedced5483f90f91e07ce62297
dnoneill <noneill@stanford.edu>
2024-06-25 16:04:24 -0400

    move home map to location_leaflet_map_component

---
+++ app/components/geoblacklight/location_leaflet_map_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Geoblacklight
+  class LocationLeafletMapComponent < ViewComponent::Base
+    def initialize(id: "map", label: nil, data_map: "", map_geometry: nil)
+      @id = id
+      @label = label
+      @data_map = data_map
+      @map_geometry = map_geometry || Settings.HOMEPAGE_MAP_GEOM
+      super
+    end
+
+    def before_render
+      @label ||= t("geoblacklight.map.label")
+    end
+  end
+end
