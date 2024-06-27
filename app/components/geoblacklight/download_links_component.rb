f7997bf542d39388772e4b8a25ed8db30a9fbdd8
Peter Mangiafico <peter@mangiafico.org>
2024-06-26 17:13:36 -0700

    pass in logic for doc being downloadable

--- app/components/geoblacklight/download_links_component.rb
+++ app/components/geoblacklight/download_links_component.rb
@@ -3,15 +3,16 @@
 module Geoblacklight
   # Display expandable file download links in sidebar
   class DownloadLinksComponent < ViewComponent::Base
-    attr_reader :document
+    attr_reader :document, :downloadable
 
-    def initialize(document:)
+    def initialize(document:, downloadable:)
       @document = document
+      @downloadable = downloadable
       super
     end
 
     def render?
-      document.direct_download.present? || document.hgl_download.present? || document.iiif_download.present? || document.download_types.present?
+      downloadable && (document.direct_download.present? || document.hgl_download.present? || document.iiif_download.present? || document.download_types.present?)
     end
   end
 end
--- spec/components/geoblacklight/download_links_component_spec.rb
+++ spec/components/geoblacklight/download_links_component_spec.rb
@@ -4,11 +4,13 @@ require "spec_helper"
 
 RSpec.describe Geoblacklight::DownloadLinksComponent, type: :component do
   subject(:rendered) do
-    render_inline_to_capybara_node(described_class.new(document: document))
+    render_inline_to_capybara_node(described_class.new(document: document, downloadable: downloadable))
   end
   let(:document) { instance_double(SolrDocument, id: 123) }
 
   context "when rendering is required" do
+    let(:downloadable) { true }
+
     before do
       allow(document).to receive(:direct_download).and_return(test: :test)
       allow(document).to receive(:hgl_download).and_return({})
@@ -23,16 +25,28 @@ RSpec.describe Geoblacklight::DownloadLinksComponent, type: :component do
   end
 
   context "when rendering is not required" do
-    before do
-      allow(document).to receive(:direct_download).and_return({})
-      allow(document).to receive(:hgl_download).and_return({})
-      allow(document).to receive(:iiif_download).and_return({})
-      allow(document).to receive(:download_types).and_return({})
+    context "because there are no download links" do
+      let(:downloadable) { true }
+      before do
+        allow(document).to receive(:direct_download).and_return({})
+        allow(document).to receive(:hgl_download).and_return({})
+        allow(document).to receive(:iiif_download).and_return({})
+        allow(document).to receive(:download_types).and_return({})
+      end
+
+      it "does not render anything" do
+        expect(rendered).not_to have_text("Download")
+        expect(rendered).not_to have_text("Export Shapefile")
+      end
     end
 
-    it "does not render anything" do
-      expect(rendered).not_to have_text("Download")
-      expect(rendered).not_to have_text("Export Shapefile")
+    context "because you aren't allowed" do
+      let(:downloadable) { false }
+
+      it "does not render anything" do
+        expect(rendered).not_to have_text("Download")
+        expect(rendered).not_to have_text("Export Shapefile")
+      end
     end
   end
 end
