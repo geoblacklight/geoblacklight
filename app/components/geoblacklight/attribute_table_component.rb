be1af10fd05d111473dadc343e56375a1477af86
Huda Khan <hudakhan@stanford.edu>
2024-07-02 14:01:39 -0600

    Moving attribute table into own component

---
+++ app/components/geoblacklight/attribute_table_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Geoblacklight
+  ##
+  # A component for rendering attribute table
+  #
+  class AttributeTableComponent < ViewComponent::Base
+    attr_reader :show_attribute_table
+    def initialize(show_attribute_table:, **)
+      @show_attribute_table = show_attribute_table
+      super
+    end
+
+    def render?
+      @show_attribute_table
+    end
+  end
+end
