0ee32752f4c0d53955080549cccc10bb21cbac2a
Huda Khan <hudakhan@stanford.edu>
2024-07-03 17:16:29 -0600

    simplifying, relying on helper in parent view

--- app/components/geoblacklight/attribute_table_component.rb
+++ app/components/geoblacklight/attribute_table_component.rb
@@ -5,13 +5,5 @@ module Geoblacklight
   # A component for rendering attribute table
   #
   class AttributeTableComponent < ViewComponent::Base
-    def initialize(show_attribute_table:, **)
-      @show_attribute_table = show_attribute_table
-      super
-    end
-
-    def render?
-      @show_attribute_table
-    end
   end
 end
--- spec/components/geoblacklight/attribute_table_component_spec.rb
+++ spec/components/geoblacklight/attribute_table_component_spec.rb
@@ -4,20 +4,12 @@ require "spec_helper"
 
 RSpec.describe Geoblacklight::AttributeTableComponent, type: :component do
   subject(:rendered) do
-    render_inline_to_capybara_node(described_class.new(**kwargs))
+    render_inline_to_capybara_node(described_class.new)
   end
 
-  describe "displays attribute table based on condition" do
-    let(:kwargs) { {show_attribute_table: true} }
-    it "displays attribute table element" do
+  describe "displays attribute table" do
+    it "displays attribute table body element" do
       expect(rendered).to have_selector(".attribute-table-body")
-    end
-  end
-
-  describe "does not display table when it should not" do
-    let(:kwargs) { {show_attribute_table: false} }
-    it "does not attribute table" do
-      expect(rendered).to_not have_selector(".attribute-table-body")
     end
   end
 end
