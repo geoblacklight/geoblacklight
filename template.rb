93e59540b6e9c38921550d69db0a6a32ed1b899e
Nick Budak <budak@stanford.edu>
2024-07-09 11:17:19 -0700

    Remove obsolete workaround from template.rb

--- template.rb
+++ template.rb
@@ -3,14 +3,6 @@
 gem "blacklight", "~> 7.0"
 gem "geoblacklight", "~> 4.0"
 
-# Hack for https://github.com/rails/rails/issues/35153
-# Adapted from https://github.com/projectblacklight/blacklight/pull/2065
-gemfile = File.expand_path("Gemfile")
-File.write(gemfile, File.open(gemfile) do |f|
-  text = f.read
-  text.gsub(/^gem 'sqlite3'$/, 'gem "sqlite3", "~> 1.3.6"')
-end)
-
 run "bundle install"
 
 generate "blacklight:install", "--devise"
