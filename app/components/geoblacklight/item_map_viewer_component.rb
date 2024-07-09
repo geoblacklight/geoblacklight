36eaadcd9787077a14eead71b2be9453919c2072
Huda Khan <hudakhan@stanford.edu>
2024-07-09 16:45:30 -0600

    incorporating components for portions of the viewer container page and adding tests

---
+++ app/components/geoblacklight/item_map_viewer_component.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Geoblacklight
+  class ItemMapViewerComponent < ViewComponent::Base
+    def initialize(document)
+      @document = document
+      @pmtiles = document.item_viewer.pmtiles
+      @cog = document.item_viewer.cog
+      @viewer_protocol = document.viewer_protocol
+      @viewer_endpoint = document.viewer_endpoint
+      @wxs_identifier = document.wxs_identifier
+      @geojson = document.geometry.geojson
+      super
+    end
+  
+
+  # Determine what to display in the page
+  # If the conditions for this being IIIF content are met, dislay the IIIF viewer
+  # Otherwise display the base viewer which will take into account if it is open layers 
+  # or generic content. 
+    def display_tag
+      return iiif_tag if iiif?
+
+      base_tag
+    end
+
+    def render?
+      @document 
+    end
+
+    private
+
+    # These are captured as openlayers_container? and iiif_manifest_viewer? in geoblacklight_helper.
+    def open_layers?
+      @pmtiles || @cog
+    end
+
+    def iiif?
+      @document&.item_viewer&.viewer_preference&.key?(:iiif_manifest)
+    end
+
+    # Generate thet tag for IIIF content
+    def iiif_tag
+      tag.div(nil, id: "clover-viewer", iiif_content: @viewer_endpoint)
+    end
+
+    # The only difference beween the open layers container and the regular leaflet container
+    # is the id. Here we check if we need to use open layers, and set the id appropriately.
+    def base_tag
+      map_id = open_layers? ? 'ol-map' : 'map'
+      tag.div(nil,
+        id: map_id,
+        data: {
+          :map => "item", :protocol => @viewer_protocol.camelize,
+          :url => @viewer_endpoint,
+          "layer-id" => @wxs_identifier,
+          "map-geom" => @geojson, 
+          "catalog-path" => helpers.search_catalog_path,
+          :available => helpers.document_available?,
+          :basemap => helpers.geoblacklight_basemap,
+          :leaflet_options => helpers.leaflet_options
+      })
+    end
+  end 
+end
---
+++ spec/components/geoblacklight/index_map_legend_component_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Geoblacklight::IndexMapLegendComponent, type: :component do
+  subject(:rendered) do
+    render_inline_to_capybara_node(described_class.new)
+  end
+
+  it "shows available map text" do
+    expect(rendered).to have_text("Green tile indicates")
+    expect(rendered).to have_selector(:css, ".index-map-legend-default")
+  end
+
+  it "shows unavailable map text" do
+    expect(rendered).to have_text("Yellow tile indicates")
+    expect(rendered).to have_selector(:css, ".index-map-legend-unavailable")
+  end
+
+  it "shows selected map text" do
+    expect(rendered).to have_text("Blue tile indicates")
+    expect(rendered).to have_selector(:css, ".index-map-legend-selected")
+  end
+end
