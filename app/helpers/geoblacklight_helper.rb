bb58e63a2d2d397b8629b6ce5149a5bf5e1e69ea
Huda Khan <hudakhan@stanford.edu>
2024-07-10 10:37:59 -0600

    removing methods we are no longer using

--- app/helpers/geoblacklight_helper.rb
+++ app/helpers/geoblacklight_helper.rb
@@ -233,57 +233,13 @@ module GeoblacklightHelper
     end
   end
 
-  def viewer_container
-    if openlayers_container?
-      ol_viewer
-    elsif iiif_manifest_container?
-      iiif_manifest_viewer
-    else
-      leaflet_viewer
-    end
-  end
-
   def openlayers_container?
     return false unless @document
     @document.item_viewer.pmtiles || @document.item_viewer.cog
   end
 
-  def leaflet_viewer
-    tag.div(nil,
-      id: "map",
-      data: {
-        :map => "item", :protocol => @document.viewer_protocol.camelize,
-        :url => @document.viewer_endpoint,
-        "layer-id" => @document.wxs_identifier,
-        "map-geom" => @document.geometry.geojson,
-        "catalog-path" => search_catalog_path,
-        :available => document_available?,
-        :basemap => geoblacklight_basemap,
-        :leaflet_options => leaflet_options
-      })
-  end
-
-  def ol_viewer
-    tag.div(nil,
-      id: "ol-map",
-      data: {
-        :map => "item", :protocol => @document.viewer_protocol.camelize,
-        :url => @document.viewer_endpoint,
-        "layer-id" => @document.wxs_identifier,
-        "map-geom" => @document.geometry.geojson,
-        "catalog-path" => search_catalog_path,
-        :available => document_available?,
-        :basemap => geoblacklight_basemap,
-        :leaflet_options => leaflet_options
-      })
-  end
-
   def iiif_manifest_container?
     return false unless @document
     @document&.item_viewer&.viewer_preference&.key?(:iiif_manifest)
-  end
-
-  def iiif_manifest_viewer
-    tag.div(nil, id: "clover-viewer", iiif_content: @document.viewer_endpoint)
   end
 end
