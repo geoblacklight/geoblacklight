4b7d7c1ca6506b934ab3387cba39b38b1c47c3c1
Nick Budak <budak@stanford.edu>
2024-07-15 11:07:38 -0700

    Start building Leaflet controller

---
+++ app/javascript/leaflet/constants.js
@@ -0,0 +1,6 @@
+export const DEFAULT_BOUNDS = [
+  [-82, -144],
+  [77, 161],
+];
+
+export const DEFAULT_OPACITY = 0.75;
--- app/javascript/modules/leaflet/viewers/pmtiles.js
+++
@@ -1,5 +0,0 @@
-import LeafletViewerBase from "./base.js";
-
-export default class LeafletViewerPmtiles extends LeafletViewerBase {
-  load() {}
-}
