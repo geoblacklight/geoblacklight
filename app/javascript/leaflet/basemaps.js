593b1e792ad0439bd499fa222ad2a87df8e7b7d7
Nick Budak <budak@stanford.edu>
2024-07-15 21:34:17 -0700

    Harmonize the Leaflet viewer and OpenLayers viewer controllers

--- app/javascript/leaflet/basemaps.js
+++ app/javascript/leaflet/basemaps.js
@@ -1,89 +1,74 @@
-import { tileLayer } from "leaflet";
-
-// NOTE: having these exported as layer objects instead of functions causes 
-// leaflet to not play nice with Turbo. Basemaps will load on initial page load,
-// but not for subsequent turbo visits. Instead, they are exported as functions
-// so that basemaps are re-instantiated each time the map is loaded.
 export default {
-  darkMatter: () => tileLayer(
-    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  positron: () => tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  positronLite: () => tileLayer(
-    'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  worldAntique: () => tileLayer(
-    'https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  worldEco: () => tileLayer(
-    'https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  flatBlue: () => tileLayer(
-    'https://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  midnightCommander: () => tileLayer(
-    'https://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}{retina}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
-      maxZoom: 18,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  openstreetmapHot: () => tileLayer(
-    'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>', 
-      maxZoom: 19,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  ),
-  openstreetmapStandard: () => tileLayer(
-    'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 19,
-      worldCopyJump: true,
-      retina: '@2x',
-      detectRetina: false
-    }
-  )
+  darkMatter: {
+    url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  positron: {
+    url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  positronLite: {
+    url: "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  worldAntique: {
+    url: "https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  worldEco: {
+    url: "https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  flatBlue: {
+    url: "https://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  midnightCommander: {
+    url: "https://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}{retina}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://carto.com/attributions">Carto</a>',
+    maxZoom: 18,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  openstreetmapHot: {
+    url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+    maxZoom: 19,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
+  openstreetmapStandard: {
+    url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
+    worldCopyJump: true,
+    retina: "@2x",
+  },
 };
