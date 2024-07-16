f6b7ba9fd2426c3ce745b660137aa898e6774e64
Nick Budak <budak@stanford.edu>
2024-07-15 21:34:17 -0700

    Harmonize the Leaflet viewer and OpenLayers viewer controllers

--- app/javascript/leaflet/constants.js
+++ app/javascript/leaflet/constants.js
@@ -1,6 +1,10 @@
-export const DEFAULT_BOUNDS = [
-  [-82, -144],
-  [77, 161],
-];
+import { LatLngBounds } from "leaflet";
+
+export const DEFAULT_BOUNDS = new LatLngBounds([
+  [-30, -100],
+  [30, 100],
+]);
 
 export const DEFAULT_OPACITY = 0.75;
+
+export const DEFAULT_BASEMAP = "positron";
