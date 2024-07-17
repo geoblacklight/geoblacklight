417bacde574adc04975662b394dba98d7e19032c
Nick Budak <budak@stanford.edu>
2024-07-17 06:56:15 -0700

    Remove converted files

--- app/javascript/leaflet/viewers/tms.js
+++
@@ -1,9 +0,0 @@
-import { tileLayer } from "leaflet";
-import LeafletViewerWms from "./wms.js";
-
-export default class LeafletViewerTms extends LeafletViewerWms {
-  addPreviewLayer() {
-    const tmsLayer = tileLayer(this.data.url, { tms: true });
-    this.overlay.addLayer(tmsLayer);
-  }
-}
--- app/javascript/leaflet/viewers/xyz.js
+++
@@ -1,9 +0,0 @@
-import { tileLayer } from "leaflet";
-import LeafletViewerWms from "./wms.js";
-
-export default class LeafletViewerXyz extends LeafletViewerWms {
-  addPreviewLayer() {
-    const xyzLayer = tileLayer(this.data.url);
-    this.overlay.addLayer(xyzLayer);
-  }
-}
