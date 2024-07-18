b0b927fb2497b667dda3c15268d708687f119896
Nick Budak <budak@stanford.edu>
2024-07-18 12:15:11 -0700

    Set up the bookmarks and homepage leaflet maps

--- app/components/geoblacklight/location_leaflet_map_component.rb
+++ app/components/geoblacklight/location_leaflet_map_component.rb
@@ -2,12 +2,20 @@
 
 module Geoblacklight
   class LocationLeafletMapComponent < ViewComponent::Base
-    def initialize(id: "leaflet-viewer", label: nil, map_geometry: Settings.HOMEPAGE_MAP_GEOM, classes: "", page: nil)
+    def initialize(
+      id: "leaflet-viewer",
+      label: nil,
+      map_geometry: Settings.HOMEPAGE_MAP_GEOM,
+      classes: "",
+      page: nil,
+      geosearch: nil
+    )
       @id = id
       @label = label
       @classes = classes
       @map_geometry = map_geometry if map_geometry != "null"
       @page = page
+      @geosearch = geosearch
       super
     end
 
@@ -22,6 +30,13 @@ module Geoblacklight
       bbox.to_geojson
     end
 
+    # Add top-level geosearch control to leaflet options if configured
+    def leaflet_options
+      options = helpers.leaflet_options
+      options["CONTROLS"] = {"Geosearch" => @geosearch} if @geosearch
+      options.to_json
+    end
+
     def viewer_tag
       tag.div(nil,
         id: @id,
@@ -34,7 +49,8 @@ module Geoblacklight
           "leaflet-viewer-basemap-value" => helpers.geoblacklight_basemap,
           "leaflet-viewer-map-geom-value" => search_bbox || @map_geometry,
           "leaflet-viewer-data-map-value" => @data_map,
-          "leaflet-viewer-options-value" => helpers.leaflet_options.to_json
+          "leaflet-viewer-options-value" => leaflet_options,
+          "leaflet-viewer-catalog-base-url-value" => (helpers.search_catalog_url if @geosearch)
         }.compact)
     end
   end
