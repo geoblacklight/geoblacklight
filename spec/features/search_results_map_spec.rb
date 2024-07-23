3edd076964e6987b13110badf012c7c4ba614e77
dnoneill <noneill@stanford.edu>
2024-07-23 10:52:04 -0400

    fix search_results_map_spec

--- spec/features/search_results_map_spec.rb
+++ spec/features/search_results_map_spec.rb
@@ -12,11 +12,13 @@ feature "search results map", js: true do
     visit root_path
     click_link "Minnesota, United States"
     expect(page).to have_css "#leaflet-viewer"
-    bbox = page.find("#leaflet-viewer")["data-js-map-render-bbox"]
+    uri = URI.parse(current_url)
+    params = URI.decode_www_form(uri.query || "").to_h
+    bbox = params["bbox"]
 
     # Example bbox for Place > Minnesota, United States:
     # "-101.90917968749999,38.75408327579141,-83.27636718749999,53.27835301753182"
-    left, bottom, right, top = bbox.split(",")
+    left, bottom, right, top = bbox.split(" ")
     expect(left.to_f).to be_within(2).of(-101)
     expect(bottom.to_f).to be_within(3).of(38)
     expect(right.to_f).to be_within(2).of(-83)
@@ -25,11 +27,14 @@ feature "search results map", js: true do
   scenario "view is scoped to Twin Cities metro area" do
     visit search_catalog_path(q: "Minneapolis")
     expect(page).to have_css "#leaflet-viewer"
-    bbox = page.find("#leaflet-viewer")["data-js-map-render-bbox"]
+    sleep 1
+    uri = URI.parse(current_url)
+    params = URI.decode_www_form(uri.query || "").to_h
+    bbox = params["bbox"]
 
     # Example bbox for q: Minneapolis
     # "-94.537353515625,44.004669106432225,-92.208251953125,45.87088761346192"
-    left, bottom, right, top = bbox.split(",")
+    left, bottom, right, top = bbox.split(" ")
     expect(left.to_f).to be_within(1).of(-94)
     expect(bottom.to_f).to be_within(1).of(44)
     expect(right.to_f).to be_within(1).of(-92)
@@ -40,11 +45,13 @@ feature "search results map", js: true do
     visit root_path
     click_link "New York, New York"
     expect(page).to have_css "#leaflet-viewer"
-    bbox = page.find("#leaflet-viewer")["data-js-map-render-bbox"]
+    uri = URI.parse(current_url)
+    params = URI.decode_www_form(uri.query || "").to_h
+    bbox = params["bbox"]
 
     # Example bbox for Place > New York, New York, United States
     # "-74.26895141601562,40.455307212131494,-73.68667602539061,40.95501133048621"
-    left, bottom, right, top = bbox.split(",")
+    left, bottom, right, top = bbox.split(" ")
     expect(left.to_f).to be_within(2).of(-74)
     expect(bottom.to_f).to be_within(2).of(40)
     expect(right.to_f).to be_within(2).of(-73)
