235f263d65b96be3f71c6c13a18edfec4c1980c1
Nick Budak <budak@stanford.edu>
2024-07-25 13:00:44 -0700

    Componentize some document show page partials

--- spec/views/catalog/_index_split.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-describe "catalog/_document_split.html.erb", type: :view do
-  # TODO: js plugin refactor really needs to be redone so that the views can be testable
-end
