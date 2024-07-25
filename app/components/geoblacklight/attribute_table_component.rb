be943a63bd372d0262ad0ffdd79356c0d9ee2463
Nick Budak <budak@stanford.edu>
2024-07-25 12:59:41 -0700

    Move helper method into a render? call

--- app/components/geoblacklight/attribute_table_component.rb
+++ app/components/geoblacklight/attribute_table_component.rb
@@ -5,5 +5,13 @@ module Geoblacklight
   # A component for rendering attribute table
   #
   class AttributeTableComponent < ViewComponent::Base
+    def initialize(document:)
+      @document = document
+      super
+    end
+
+    def render?
+      helpers.document_available?(@document) && @document.inspectable?
+    end
   end
 end
