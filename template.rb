2abb47ee9b432105c633140116534770b7d507e3
Eliot Jordan <eliotj@princeton.edu>
2024-07-29 16:33:37 -0500

    Add ability to run template.rb with a branch

--- template.rb
+++ template.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 gem "blacklight", "~> 7.0"
-gem "geoblacklight", "~> 4.0"
 
-# Hack for https://github.com/rails/rails/issues/35153
-# Adapted from https://github.com/projectblacklight/blacklight/pull/2065
-gemfile = File.expand_path("Gemfile")
-File.write(gemfile, File.open(gemfile) do |f|
-  text = f.read
-  text.gsub(/^gem 'sqlite3'$/, 'gem "sqlite3", "~> 1.3.6"')
-end)
+# Install latest version of geoblacklight gem if running
+# generator with a development branch.
+if ENV["BRANCH"]
+  gem "geoblacklight", github: "geoblacklight/geoblacklight", branch: ENV["BRANCH"]
+else
+  gem "geoblacklight", "~> 4.0"
+end
 
 run "bundle install"
 
 generate "blacklight:install", "--devise"
 generate "geoblacklight:install", "-f"
+
+# Install latest version of geoblacklight frontend javascript if
+# running generator with a development branch
+if ENV["BRANCH"]
+  run "yarn add @geoblacklight/frontend@latest"
+end
 
 rake "db:migrate"
