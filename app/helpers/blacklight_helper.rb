d0c6187704f9bd8501e28e94d7b90f8ee2710af4
Nick Budak <budak@stanford.edu>
2024-07-29 16:09:23 -0700

    Convert our custom helper behavior into a component method
    This removes the GeoblacklightHelperBehavior that was created
    to generate our search result preview descriptions and makes it
    into a tested component method instead, so that there's less
    overriding of Blacklight.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -2,7 +2,6 @@
 
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
-  include Geoblacklight::GeoblacklightHelperBehavior
   include Blacklight::CatalogHelperBehavior
 
   def render_document_sidebar_partial(document = @document)
