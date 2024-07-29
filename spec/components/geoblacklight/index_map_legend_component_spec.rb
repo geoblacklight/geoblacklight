d537ba118c1e4bb8aaf2b2f67bee51ef6da06683
Nick Budak <budak@stanford.edu>
2024-07-29 10:37:52 -0700

    Adjust a test for index map legend component

--- spec/components/geoblacklight/index_map_legend_component_spec.rb
+++ spec/components/geoblacklight/index_map_legend_component_spec.rb
@@ -9,18 +9,34 @@ RSpec.describe Geoblacklight::IndexMapLegendComponent, type: :component do
     render_inline_to_capybara_node(described_class.new(document:))
   end
 
-  it "shows available map text" do
-    expect(rendered).to have_text("Green tile indicates")
-    expect(rendered).to have_selector(:css, ".index-map-legend-default")
+  context "when the document has an index map" do
+    before do
+      allow(document).to receive_message_chain(:item_viewer, :index_map).and_return(true)
+    end
+
+    it "shows available map text" do
+      expect(rendered).to have_text("Green tile indicates")
+      expect(rendered).to have_selector(:css, ".index-map-legend-default")
+    end
+
+    it "shows unavailable map text" do
+      expect(rendered).to have_text("Yellow tile indicates")
+      expect(rendered).to have_selector(:css, ".index-map-legend-unavailable")
+    end
+
+    it "shows selected map text" do
+      expect(rendered).to have_text("Blue tile indicates")
+      expect(rendered).to have_selector(:css, ".index-map-legend-selected")
+    end
   end
 
-  it "shows unavailable map text" do
-    expect(rendered).to have_text("Yellow tile indicates")
-    expect(rendered).to have_selector(:css, ".index-map-legend-unavailable")
-  end
+  context "when the document does not have an index map" do
+    before do
+      allow(document).to receive_message_chain(:item_viewer, :index_map).and_return(false)
+    end
 
-  it "shows selected map text" do
-    expect(rendered).to have_text("Blue tile indicates")
-    expect(rendered).to have_selector(:css, ".index-map-legend-selected")
+    it "does not render" do
+      expect(rendered.text.strip).to be_empty
+    end
   end
 end
