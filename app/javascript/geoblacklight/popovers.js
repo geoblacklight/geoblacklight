fa0db9bffee6836067cfce4d3a3c1cbfc28b154a
Nick Budak <budak@stanford.edu>
2024-07-29 11:02:31 -0700

    Adjust data-bs-toggle and data-bs-target attributes

--- app/javascript/geoblacklight/popovers.js
+++ app/javascript/geoblacklight/popovers.js
@@ -1,6 +1,6 @@
 // Initialize popovers
 export default function initializePopovers() {
-  const popoverElements = document.querySelectorAll('[data-toggle="popover"]'); // Bootstrap 5 uses 'data-bs-toggle'
+  const popoverElements = document.querySelectorAll('[data-bs-toggle="popover"]'); // Bootstrap 5 uses 'data-bs-toggle'
   popoverElements.forEach((element) => {
     const popover = new bootstrap.Popover(element, {
       trigger: "hover",
