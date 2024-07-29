0310ca42a6016f87043a7d43d31cee5c088b60f6
mlongley <mlongley@stanford.edu>
2024-07-29 14:39:03 -0400

    Remove log statements

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["citation", "alert"];
 
-    connect() {
-        console.log('Hello, clipboard!')
-    }
-
     async copyToClipboard() {
         let text = this.citationTarget.textContent.trim();
         await navigator.clipboard.writeText(text)
