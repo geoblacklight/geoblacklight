b1703e4aac243a774387274c0d3c66920246c885
Nick Budak <budak@stanford.edu>
2024-07-30 16:47:10 -0700

    Support for importmaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,20 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import { exec } from "child_process";
 
 export default defineConfig(() => {
   return {
-    publicDir: 'app/assets',
     build: {
-      outDir: 'dist',
-      copyPublicDir: true,
+      outDir: "app/assets/javascripts/geoblacklight",
       emptyOutDir: true,
-      manifest: true,
-      minify: false,
       sourcemap: true,
       lib: {
-        entry: resolve(__dirname, "app/javascript/index.js"),
+        entry: resolve(__dirname, "app/javascript/geoblacklight/index.js"),
         name: "@geoblacklight/frontend",
         fileName: "geoblacklight",
       },
     },
     test: {
-      environment: 'jsdom',
-    }
+      environment: "jsdom",
+    },
   };
 });
