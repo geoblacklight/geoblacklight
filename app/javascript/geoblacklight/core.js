f42f15b10a2cfb9f4760278a2bfecf8d12852737
Nick Budak <budak@stanford.edu>
2024-07-31 10:38:08 -0700

    Use a top-level export style inspired by Blacklight for JS

--- app/javascript/geoblacklight/core.js
+++ app/javascript/geoblacklight/core.js
@@ -13,42 +13,55 @@ import initializeTruncation from "./truncation";
 import initializeDownloads from "./downloads";
 import initializeMetadataDownload from "./metadata_download";
 
-// Inspired by https://github.com/projectblacklight/blacklight/blob/main/app/javascript/blacklight/core.js
-const Geoblacklight = function () {
+// Inspired by Blacklight's javascript/blacklight/core.js
+const Geoblacklight = (function () {
+  const callbacks = [];
   return {
-    // Everything that should be set up when the Geoblacklight JS is activated
-    activate: function () {
-      if (typeof Stimulus !== "undefined") {
-        Stimulus.register("openlayers-viewer", OpenlayersViewerController);
-        Stimulus.register("clover-viewer", CloverViewerController);
-        Stimulus.register("oembed-viewer", OembedViewerController);
-        Stimulus.register("leaflet-viewer", LeafletViewerController);
-        Stimulus.register("search-results", SearchResultsController);
-        Stimulus.register("downloads", DownloadsController);
-        Stimulus.register("clipboard", ClipboardController);
-      }
-      initializeRelations();
-      initializeTruncation();
-      initializePopovers();
-      initializeTooltips();
-      initializeDownloads();
-      initializeMetadataDownload();
+    // Hook: pass a callback to add it to the activation stack
+    onLoad: function (callback) {
+      callbacks.push(callback);
     },
+
+    // Activate all stored callbacks
+    activate: function (event) {
+      callbacks.forEach((callback) => {
+        callback(event)
+      });
+    },
+
     // Define hooks that will trigger the activation of the Geoblacklight JS
     listeners: function () {
-      const listeners = [];
-      if (typeof Turbo !== "undefined") {
-        listeners.push("turbo:load", "turbo:frame-load");
-      } else {
-        listeners.push("DOMContentLoaded");
-      }
-      return listeners;
+      if (typeof Turbo !== "undefined") return ["turbo:load", "turbo:frame-load"];
+      else return ["DOMContentLoaded"];
     },
   };
-};
+})();
 
+// Add event listeners that call activate() for each event type
 Geoblacklight.listeners().forEach((listener) =>
-  document.addEventListener(listener, Geoblacklight.activate)
+  document.addEventListener(listener, (event) => Geoblacklight.activate(event))
 );
+
+// Default activation stack
+Geoblacklight.onLoad(initializePopovers);
+Geoblacklight.onLoad(initializeRelations);
+Geoblacklight.onLoad(initializeTooltips);
+Geoblacklight.onLoad(initializeTruncation);
+Geoblacklight.onLoad(initializeDownloads);
+Geoblacklight.onLoad(initializeMetadataDownload);
+
+// Register our Stimulus controllers
+if (typeof Stimulus !== "undefined") {
+  Stimulus.register("openlayers-viewer", OpenlayersViewerController);
+  Stimulus.register("clover-viewer", CloverViewerController);
+  Stimulus.register("oembed-viewer", OembedViewerController);
+  Stimulus.register("leaflet-viewer", LeafletViewerController);
+  Stimulus.register("search-results", SearchResultsController);
+  Stimulus.register("downloads", DownloadsController);
+  Stimulus.register("clipboard", ClipboardController);
+}
+else {
+  console.error("Couldn't find Stimulus. Check instructions at https://github.com/hotwired/stimulus-rails");
+}
 
 export default Geoblacklight;
