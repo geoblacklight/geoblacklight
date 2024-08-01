8384cf69c221971aae86278a2742a7427cfe1864
dnoneill <noneill@stanford.edu>
2024-08-01 16:36:14 -0400

    import bootstrap methods in popover/tooltip

--- app/javascript/geoblacklight/popovers.js
+++ app/javascript/geoblacklight/popovers.js
@@ -1,8 +1,9 @@
+import { Popover } from 'bootstrap';
 // Initialize popovers
 export default function initializePopovers() {
   const popoverElements = document.querySelectorAll('[data-bs-toggle="popover"]'); // Bootstrap 5 uses 'data-bs-toggle'
   popoverElements.forEach((element) => {
-    const popover = new bootstrap.Popover(element, {
+    const popover = Popover(element, {
       trigger: "hover",
     });
   });
