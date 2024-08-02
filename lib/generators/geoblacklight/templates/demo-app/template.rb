e79904ee614665ef9a1b9d72c58c2d972f2462c5
Eliot Jordan <eliotj@princeton.edu>
2024-08-02 13:49:19 -0500

    Udpate demo template app

--- lib/generators/geoblacklight/templates/demo-app/template.rb
+++ lib/generators/geoblacklight/templates/demo-app/template.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-gem "blacklight", "~> 7.0"
+gem "blacklight", "~> 8.0"
 
 # Install latest version of geoblacklight gem if running
 # generator with a development branch.
