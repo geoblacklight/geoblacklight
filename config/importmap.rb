7dbd750c4469345f229025fe29b06d5d81350101
Nick Budak <budak@stanford.edu>
2024-08-05 09:19:09 -0700

    Add importmap and asset config to the Gem engine

---
+++ config/importmap.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+pin_all_from Geoblacklight::Engine.root.join("app/javascript/geoblacklight"), under: "geoblacklight"
--- lib/geoblacklight/engine.rb
+++ lib/geoblacklight/engine.rb
@@ -18,6 +18,25 @@ module Geoblacklight
       end
     end
 
+    PRECOMPILE_ASSETS = %w[favicon.ico geoblacklight/geoblacklight.umd.js geoblacklight/geoblacklight.umd.js.map geoblacklight/geoblacklight.mjs geoblacklight/geoblacklight.mjs.map].freeze
+
+    # Make the source JS and CSS available to built apps
+    initializer "geoblacklight.assets" do |app|
+      # If we're using Vite, there won't be any asset configuration
+      next unless app.config.respond_to?(:assets)
+
+      app.config.assets.paths << Geoblacklight::Engine.root.join("app/javascript")
+      app.config.assets.paths << Geoblacklight::Engine.root.join("app/assets/stylesheets")
+      app.config.assets.precompile += Geoblacklight::Engine::PRECOMPILE_ASSETS
+    end
+
+    # Add the Geoblacklight importmap to the built app's importmap
+    initializer "geoblacklight.importmap", before: "importmap" do |app|
+      next unless app.config.respond_to?(:importmap)
+
+      app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+    end
+
     config.to_prepare do
       unless SearchHistoryController.helpers.is_a?(Geoblacklight::ViewHelperOverride)
         SearchHistoryController.send(:helper, Geoblacklight::ViewHelperOverride)
