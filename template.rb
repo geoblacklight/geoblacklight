63fd57e5f8af21c8a478e583f433ce3c6db3e0bf
Nick Budak <budak@stanford.edu>
2024-08-06 14:59:48 -0700

    Force engine_cart and template.rb builds to use propshaft

--- template.rb
+++ template.rb
@@ -2,6 +2,10 @@
 
 gem "blacklight", "~> 8.0"
 
+# Ensure the app generates with Propshaft; sprockets is no longer supported
+# https://github.com/geoblacklight/geoblacklight/issues/1265
+ENV["ENGINE_CART_RAILS_OPTIONS"] = ENV["ENGINE_CART_RAILS_OPTIONS"].to_s + " -a propshaft"
+
 # Install latest version of geoblacklight gem if running
 # generator with a development branch.
 if ENV["BRANCH"]
