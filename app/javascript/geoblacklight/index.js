8933d4491f0bed49746bf7f17ef51d9e1c1bef8e
Nick Budak <budak@stanford.edu>
2024-08-06 14:49:44 -0700

    Use bare imports to make importmaps work

--- app/javascript/geoblacklight/index.js
+++ app/javascript/geoblacklight/index.js
@@ -1,18 +1,18 @@
 // Stimulus controllers
-import OpenlayersViewerController from "./controllers/openlayers_viewer_controller";
-import CloverViewerController from "./controllers/clover_viewer_controller";
-import OembedViewerController from "./controllers/oembed_viewer_controller";
-import LeafletViewerController from "./controllers/leaflet_viewer_controller";
-import SearchResultsController from "./controllers/search_results_controller";
-import DownloadsController from "./controllers/downloads_controller";
-import ClipboardController from "./controllers/clipboard_controller";
+import OpenlayersViewerController from "geoblacklight/controllers/openlayers_viewer_controller";
+import CloverViewerController from "geoblacklight/controllers/clover_viewer_controller";
+import OembedViewerController from "geoblacklight/controllers/oembed_viewer_controller";
+import LeafletViewerController from "geoblacklight/controllers/leaflet_viewer_controller";
+import SearchResultsController from "geoblacklight/controllers/search_results_controller";
+import DownloadsController from "geoblacklight/controllers/downloads_controller";
+import ClipboardController from "geoblacklight/controllers/clipboard_controller";
 
 // Other modules
-import openLayersBasemaps from "./openlayers/basemaps";
-import leafletBasemaps from "./leaflet/basemaps";
+import openLayersBasemaps from "geoblacklight/openlayers/basemaps";
+import leafletBasemaps from "geoblacklight/leaflet/basemaps";
 
 // Core
-import Core from "./core";
+import Core from "geoblacklight/core";
 
 export default {
   OpenlayersViewerController,
