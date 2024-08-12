dabdb18aaaf1e0cdcb2d97f9f0abd4f46149ce93
Nick Budak <thatbudakguy@gmail.com>
2024-08-12 08:39:20 -0700

    Revert to using `yarn link` so changes are picked up in development (#1479)

--- lib/generators/geoblacklight/install_generator.rb
+++ lib/generators/geoblacklight/install_generator.rb
@@ -110,19 +110,35 @@ module Geoblacklight
       copy_file "package.json", "package.json"
     end
 
+    # Pick a version of the frontend asset package and install it.
+    def add_frontend
+      # If in local development or CI, install the version we made linkable in
+      # the test app generator. This will make it so changes made in the outer
+      # directory are picked up automatically, like a symlink. Note that this
+      # does NOT install the dependencies of the package, so we have to make
+      # sure to install those separately. See:
+      # https://classic.yarnpkg.com/lang/en/docs/cli/link/
+      # https://github.com/yarnpkg/yarn/issues/2914
+      if options[:test]
+        run "yarn link @geoblacklight/frontend"
+
+      # If a branch was specified (e.g. you are running a template.rb build
+      # against a test branch), use the latest version available on npm
+      elsif ENV["BRANCH"]
+        run "yarn add @geoblacklight/frontend@latest"
+
+      # Otherwise, pick the version from npm that matches our Geoblacklight
+      # gem version
+      else
+        run "yarn add @geoblacklight/frontend@#{Geoblacklight::VERSION}"
+      end
+    end
+
     # Vite - Config files
     def copy_config_vite_json
       copy_file "vite.json", "config/vite.json"
       copy_file "vite.config.ts", "vite.config.ts"
       run "yarn install"
-    end
-
-    def add_frontend
-      if options[:test]
-        run "yarn add file:#{Geoblacklight::Engine.root}"
-      else
-        run "yarn add @geoblacklight/frontend@#{Geoblacklight::VERSION}"
-      end
     end
 
     # Run bundle with vite install
