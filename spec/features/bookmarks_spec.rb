f9c00c0c189d3cba2e7cc1b06046c350d4bb6b83
Nick Budak <thatbudakguy@gmail.com>
2024-08-15 13:27:35 -0700

    Change tests to stop using NYU fixture (#1526)
    This fixes an HTTP timeout error that could arise when an NYU
    fixture was queried over WMS and didn't respond in time, causing
    tests to fail.
    
    This commit switches the two tests that used the fixture to use
    other, equivalent fixtures to avoid the timeouts.
    
    See also #1492.

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 feature "Blacklight Bookmarks" do
   scenario "index has created bookmarks" do
-    visit solr_document_path "nyu-2451-34564"
+    visit solr_document_path "harvard-g7064-s2-1834-k3"
     click_button "Bookmark"
     visit bookmarks_path
     expect(page).to have_css ".document", count: 1
