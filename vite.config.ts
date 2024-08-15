bb3f6743a6da770bb74c7725537205de5d3ed761
Nick Budak <budak@stanford.edu>
2024-08-15 11:02:39 -0700

    Rewrite bare imports to relative paths for bundler compatibility
    See #1519

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,35 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import { exec } from "child_process";
+import includePaths from "rollup-plugin-includepaths";
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'app/assets/javascripts/geoblacklight',
+      outDir: "app/assets/javascripts/geoblacklight",
       emptyOutDir: true,
       manifest: true,
       minify: false,
       sourcemap: true,
       lib: {
-        entry: resolve(__dirname, "app/javascript/index.js"),
+        entry: resolve(__dirname, "app/javascript/geoblacklight/index.js"),
         name: "@geoblacklight/frontend",
         fileName: "geoblacklight",
       },
+      // Rewrite importmap-friendly bare specifiers like geoblacklight/foo/bar
+      // to bundler-friendly local paths like ./foo/bar
+      rollupOptions: {
+        plugins: [
+          includePaths({
+            paths: ["app/javascript"],
+            extensions: [".js"],
+            include: {},
+            external: [],
+          }),
+        ],
+      },
     },
     test: {
-      environment: 'jsdom',
-    }
+      environment: "jsdom",
+    },
   };
 });
