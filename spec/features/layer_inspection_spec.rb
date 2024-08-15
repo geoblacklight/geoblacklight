2d674c0fec0ba922bce43746c0892093c7eaf491
Nick Budak <budak@stanford.edu>
2024-08-15 13:13:32 -0700

    Change tests to stop using NYU fixture
    This fixes an HTTP timeout error that could arise when an NYU
    fixture was queried over WMS and didn't respond in time, causing
    tests to fail.
    
    This commit switches the two tests that used the fixture to use
    other, equivalent fixtures to avoid the timeouts.
    
    See also #1492.

--- spec/features/layer_inspection_spec.rb
+++ spec/features/layer_inspection_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 feature "Layer inspection", js: true do
   scenario "clicking map should trigger inspection" do
-    visit solr_document_path("nyu-2451-34564")
+    visit solr_document_path("tufts-cambridgegrid100-04")
     expect(page).to have_css("th", text: "Attribute")
     find("#leaflet-viewer").click
     expect(page).not_to have_css("td.default-text")
