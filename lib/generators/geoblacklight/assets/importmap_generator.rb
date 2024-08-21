aff21172b15e05f2968db306544a6f83f527bd4a
Nick Budak <budak@stanford.edu>
2024-08-21 15:43:48 -0700

    Use dartsass-rails instead of cssbundling-rails

--- lib/generators/geoblacklight/assets/importmap_generator.rb
+++ lib/generators/geoblacklight/assets/importmap_generator.rb
@@ -11,63 +11,15 @@ module Geoblacklight
 
       desc <<-DESCRIPTION
         This generator sets up the app to use Importmaps to manage the javascript,
-        and cssbundling-rails to manage the styles.
+        and dartsass-rails to manage the styles.
 
         SCSS sources from Bootstrap and Blacklight are installed via yarn and
-        built into a single CSS stylesheet by cssbundling-rails.
+        built into a single CSS stylesheet.
 
-        JS sources from Blacklight and Geoblacklight are delivered unbundled as
+        JS sources from the Blacklight and Geoblacklight gems are delivered as
         ES modules via importmap, and their dependencies are pinned to versions
         delivered via CDN.
       DESCRIPTION
-
-      # Add our package.json and install dependencies
-      # This is used so that we get local copies of the SCSS needed to build the
-      # styles; JS all comes from the importmap
-      def install_dependencies
-        copy_file "package.json", "package.json"
-        run "yarn install"
-      end
-
-      # sass-rails and sassc-rails are deprecated
-      # See: https://github.com/geoblacklight/geoblacklight/issues/1265
-      def remove_sass_gems
-        gsub_file "Gemfile", /gem "sassc-rails"/, '# \0'
-        gsub_file "Gemfile", /gem "sass-rails"/, '# \0'
-      end
-
-      # Add cssbundling-rails for sass compilation
-      def install_cssbundling_rails
-        gem "cssbundling-rails"
-        Bundler.with_clean_env { run "bundle install" }
-        rails_command "css:install:sass"
-      end
-
-      # Configure cssbundling to bundle geoblacklight's sass for us
-      def setup_cssbundling_entrypoints
-        gsub_file "package.json", "application.sass.scss", "geoblacklight.scss"
-        gsub_file "package.json", "application.css", "geoblacklight.css"
-        gsub_file "package.json", " --no-source-map", ""  # turn sourcemaps on
-      end
-
-      # Add Geoblacklight's stylesheet and remove the default Blacklight one
-      def add_stylesheets
-        remove_file "app/assets/stylesheets/blacklight.scss"
-        copy_file "assets/_customizations.scss", "app/assets/stylesheets/_customizations.scss"
-        copy_file "assets/geoblacklight.scss", "app/assets/stylesheets/geoblacklight.scss"
-      end
-
-      # Add the main CSS stylesheet and remove the default one
-      def add_style_entrypoint
-        remove_file "app/assets/stylesheets/application.css"
-        remove_file "app/assets/stylesheets/application.sass.scss"
-        copy_file "assets/application.css", "app/assets/stylesheets/application.css"
-      end
-
-      # Load the Geoblacklight logo from the gem instead of the frontend package
-      def update_gbl_logo_path
-        gsub_file "app/assets/stylesheets/_customizations.scss", "@geoblacklight/frontend/app/assets/", ""
-      end
 
       # If this is a local dev/test build, symlink the frontend package so we
       # can reference its stylesheets in development
@@ -75,27 +27,48 @@ module Geoblacklight
         run "yarn link @geoblacklight/frontend" if options[:test]
       end
 
-      # Update the main javascript entrypoint
-      def update_js_entrypoint
-        imports = <<~JS
-          import "bootstrap"
-          import "blacklight"
-          import "geoblacklight"
-        JS
-
-        append_to_file "app/javascript/application.js", imports, before: "import \"controllers\""
+      # Add the customization overrides and insert before bootstrap import
+      def add_customizations
+        copy_file "assets/_customizations.scss", "app/assets/stylesheets/_customizations.scss"
+        gsub_file "app/assets/stylesheets/_customizations.scss", "@geoblacklight/frontend/app/assets/", ""
+        insert_into_file "app/assets/stylesheets/application.bootstrap.scss", "@import 'customizations';\n", before: "@import 'bootstrap/scss/bootstrap';"
       end
 
-      # Add a pin for bootstrap to the generated importmap
+      # Add CDN imports for CSS files used by Geoblacklight (leaflet, openlayers)
+      def add_leaflet_ol_css_cdn
+        insert_into_file "app/assets/stylesheets/application.bootstrap.scss", before: "@import 'customizations';\n" do
+          <<~SCSS
+            @import url("https://cdn.skypack.dev/leaflet@1.9.4/dist/leaflet.css");
+            @import url("https://cdn.jsdelivr.net/npm/leaflet-fullscreen@1.0.2/dist/leaflet.fullscreen.css");
+            @import url("https://cdn.skypack.dev/ol@8.1.0/ol.css");
+          SCSS
+        end
+      end
+
+      # Add an import for Geoblacklight's stylesheet
+      def add_geoblacklight_styles
+        append_to_file "app/assets/stylesheets/application.bootstrap.scss", "@import '@geoblacklight/frontend/app/assets/stylesheets/geoblacklight/geoblacklight';"
+      end
+
+      # Ensure import of Blacklight's JS uses the name that importmap has pinned
+      def update_blacklight_import
+        gsub_file "app/javascript/application.js", "blacklight-frontend", "blacklight"
+      end
+
+      # Import Geoblacklight's JS using the name that importmap has pinned
+      def add_geoblacklight_js
+        append_to_file "app/javascript/application.js", "import Geoblacklight from \"geoblacklight\";"
+      end
+
+      # Add pins for application dependencies to the importmap
       def update_importmap
-        append_to_file "config/importmap.rb", "pin \"bootstrap\", to: \"https://cdn.skypack.dev/bootstrap@5.3.3\"\n"
-      end
-
-      # Build the styles; remove the built application.css because we
-      # just want to reference the version in app/assets/stylesheets
-      def build_styles
-        run "yarn build:css"
-        remove_file "app/assets/builds/application.css"
+        gsub_file "config/importmap.rb", "bootstrap.min.js", "https://cdn.skypack.dev/bootstrap@5.3.3"
+        append_to_file "config/importmap.rb" do
+          <<~CONTENT
+            pin "@github/auto-complete-element", to: "https://cdn.skypack.dev/@github/auto-complete-element"
+            pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/umd/popper.min.js"
+          CONTENT
+        end
       end
     end
   end
