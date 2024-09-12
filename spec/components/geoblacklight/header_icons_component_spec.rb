7d8804b9bb7cdac7f71fc6e46a7af8d520d6dbac
Nick Budak <budak@stanford.edu>
2024-09-12 13:19:16 -0700

    Choose a more specific icon based on resource type even if there are multiple
    See https://github.com/sul-dlss/earthworks/issues/1398

--- spec/components/geoblacklight/header_icons_component_spec.rb
+++ spec/components/geoblacklight/header_icons_component_spec.rb
@@ -6,40 +6,51 @@ RSpec.describe Geoblacklight::HeaderIconsComponent, type: :component do
   subject(:rendered) do
     render_inline_to_capybara_node(described_class.new(document: document))
   end
+
   let(:document) { SolrDocument.new(document_attributes) }
+  let(:resource_class) { [] }
+  let(:resource_type) { [] }
   let(:document_attributes) do
     {
-      "gbl_resourceClass_sm" => ["Datasets"].to_json,
-      "gbl_resourceType_sm" => resource_type_list.to_json,
+      "gbl_resourceClass_sm" => resource_class,
+      "gbl_resourceType_sm" => resource_type,
       "schema_provider_s" => "Stanford",
       "dct_accessRights_s" => "Public"
     }
   end
 
-  context "when there is a resource type with icon" do
-    let(:resource_type_list) { ["Polygon"] }
-    it "renders 3 icons" do
-      expect(rendered).to have_css(".blacklight-icons-polygon")
-      expect(rendered).to have_css(".blacklight-icons-stanford")
-      expect(rendered).to have_css(".blacklight-icons-public")
+  it "renders the provider icon" do
+    expect(rendered).to have_css(".blacklight-icons-stanford")
+  end
+
+  it "renders the access rights icon" do
+    expect(rendered).to have_css(".blacklight-icons-public")
+  end
+
+  context "when there are no resource types with icons" do
+    let(:resource_class) { ["Datasets"] }
+    let(:resource_type) { ["Census data", "Statistical maps"] }
+
+    it "renders the resource class icon" do
+      expect(rendered).to have_css(".blacklight-icons-datasets")
     end
   end
 
-  context "when there is a resource type without icon" do
-    let(:resource_type_list) { ["Index Maps"] }
-    it "renders 3 icons" do
-      expect(rendered).to have_css(".blacklight-icons-datasets")
-      expect(rendered).to have_css(".blacklight-icons-stanford")
-      expect(rendered).to have_css(".blacklight-icons-public")
+  context "when there is a 'data' resource type with an icon" do
+    let(:resource_class) { ["Datasets"] }
+    let(:resource_type) { ["Raster data"] }
+
+    it "renders the resource type icon" do
+      expect(rendered).to have_css(".blacklight-icons-raster")
     end
   end
 
-  context "when the resource type is nil" do
-    let(:resource_type_list) { [nil] }
-    it "renders 3 icons without errors" do
-      expect(rendered).to have_css(".blacklight-icons-datasets")
-      expect(rendered).to have_css(".blacklight-icons-stanford")
-      expect(rendered).to have_css(".blacklight-icons-public")
+  context "when there are multiple resource types and one has an icon" do
+    let(:resource_class) { ["Datasets"] }
+    let(:resource_type) { ["Nautical charts", "Raster data"] }
+
+    it "renders the resource type that has an icon" do
+      expect(rendered).to have_css(".blacklight-icons-raster")
     end
   end
 end
