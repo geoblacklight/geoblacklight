ae429605178eec9ced7f27fd59bfe570890ab9e4
Eliot Jordan <eliotj@princeton.edu>
2024-09-21 14:54:03 -0500

    Add test for citation clipboard contents

--- spec/features/citations_spec.rb
+++ spec/features/citations_spec.rb
@@ -2,11 +2,15 @@
 
 require "spec_helper"
 
-feature "Blacklight Citation" do
+feature "Blacklight Citation", js: true do
   scenario "index has created citations" do
     sign_in
     visit "/catalog/princeton-1r66j405w"
     click_link "Cite"
-    expect(page).to have_text "Copy Citation"
+    page.driver.browser.add_permission('clipboard-read', 'granted')
+    page.driver.browser.add_permission('clipboard-write', 'granted')
+    click_button "Copy Citation"
+    clip_text = page.evaluate_async_script('navigator.clipboard.readText().then(arguments[0])')
+    expect(clip_text).to include("Sanborn Map Company")
   end
 end
