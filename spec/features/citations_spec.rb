805966fcf2885a22007033a9ab4131c684e54a26
Nick Budak <budak@stanford.edu>
2024-09-23 15:43:41 -0700

    Fix clipboard copy functionality for web services modal
    This fixes the clipboard copying logic for the web services modal
    and adds tests, inspired by #1613.
    
    Closes #1550

--- spec/features/citations_spec.rb
+++ spec/features/citations_spec.rb
@@ -3,10 +3,14 @@
 require "spec_helper"
 
 feature "Blacklight Citation" do
-  scenario "index has created citations" do
-    sign_in
+  scenario "citations can be copied", js: true do
+    sign_in # NOTE: this seems to be required for clipboard permissions to be granted succesfully
+    page.driver.browser.add_permission("clipboard-read", "granted")
+    page.driver.browser.add_permission("clipboard-write", "granted")
     visit "/catalog/princeton-1r66j405w"
     click_link "Cite"
-    expect(page).to have_text "Copy Citation"
+    click_button "Copy Citation"
+    clip_text = page.evaluate_async_script("navigator.clipboard.readText().then(arguments[0])")
+    expect(clip_text).to include("Sanborn Map Company. Princeton, Mercer County, New Jersey.")
   end
 end
