9f7dbb4c36dc0b8e88fb9be5282d7b1663858829
dnoneill <noneill@stanford.edu>
2025-03-27 12:18:56 -0400

    refactor sidebar_component

--- app/components/geoblacklight/document/sidebar_component.rb
+++ app/components/geoblacklight/document/sidebar_component.rb
@@ -3,6 +3,21 @@
 module Geoblacklight
   module Document
     class SidebarComponent < Blacklight::Document::SidebarComponent
+      def render_static_map_component
+        render Geoblacklight::StaticMapComponent.new(document:)
+      end
+
+      def sidebar_buttons
+        [
+          Geoblacklight::WebServicesLinkComponent.new(document:),
+          Geoblacklight::DownloadLinksComponent.new(document:),
+          Geoblacklight::LoginLinkComponent.new(document:)
+        ]
+      end
+
+      def render_more_like_this
+        render Blacklight::Document::MoreLikeThisComponent.new(document:)
+      end
     end
   end
 end
