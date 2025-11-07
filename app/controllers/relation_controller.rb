1b5a0ea588f16e46a0d9f2dcf8f78704c0b371d9
Nick Budak <budak@stanford.edu>
2025-11-07 10:03:18 -0800

    Remove unused code

--- app/controllers/relation_controller.rb
+++ app/controllers/relation_controller.rb
@@ -3,7 +3,6 @@
 class RelationController < ApplicationController
   include Blacklight::Configurable
 
-  # include Blacklight::SearchHelper
   copy_blacklight_config_from(CatalogController)
 
   def index
