07e42cefee6e7bb9c8b02adcef95859b6a985de0
Nick Budak <thatbudakguy@gmail.com>
2025-11-13 13:20:34 -0800

    Fix installation order in template.rb builds (#1689)
    * Simplify the template.rb file
    
    * Install the frontend package regardless of asset pipeline
    
    Both asset pipelines use it, so this just installs it before
    invoking either pipeline.

--- lib/generators/geoblacklight/assets_generator.rb
+++ lib/generators/geoblacklight/assets_generator.rb
@@ -7,6 +7,21 @@ module Geoblacklight
     class_option :"asset-pipeline", type: :string, default: ENV.fetch("ASSET_PIPELINE", "vite"), desc: "Choose the asset pipeline to use (vite or importmap)"
     class_option :test, type: :boolean, default: ENV.fetch("CI", false), aliases: "-t", desc: "Indicates that app will be installed in a test environment"
 
+    # Pick a version of the frontend asset package and install it.
+    #
+    # If a branch was specified (e.g. you are running a template.rb build
+    # against a test branch), use the latest version available on npm.
+    #
+    # Otherwise, pick the version from npm that matches our Geoblacklight
+    # gem version.
+    def add_frontend
+      if ENV["BRANCH"]
+        run "yarn add @geoblacklight/frontend@latest"
+      else
+        run "yarn add @geoblacklight/frontend@#{Geoblacklight::VERSION}"
+      end
+    end
+
     def run_asset_pipeline_specific_generator
       generated_options = "--test=true" if options[:test]
       generator = if options[:"asset-pipeline"]
--- template.rb
+++ template.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
-gem "blacklight", "~> 8.0"
-
-# Ensure the app generates with Propshaft; sprockets is no longer supported
-# https://github.com/geoblacklight/geoblacklight/issues/1265
-ENV["ENGINE_CART_RAILS_OPTIONS"] = ENV["ENGINE_CART_RAILS_OPTIONS"].to_s + " -a propshaft --css bootstrap"
-
-# Use importmaps for JS if requested, otherwise default to vite
-ENV["ENGINE_CART_RAILS_OPTIONS"] += if ENV["ASSET_PIPELINE"] == "importmap"
-  " --js importmap"
-else
-  " --js rollup"
-end
-
-# Install latest version of geoblacklight gem if running
-# generator with a development branch.
+# Set BRANCH=<branch_name> to install from a specific branch
 if ENV["BRANCH"]
   gem "geoblacklight", github: "geoblacklight/geoblacklight", branch: ENV["BRANCH"]
 else
-  gem "geoblacklight", "~> 5.0"
+  gem "geoblacklight"
 end
 
 run "bundle install"
 
-generate "blacklight:install", "--devise", "--skip-solr"
-generate "geoblacklight:install", "-f"
-
-rake "db:migrate"
+after_bundle do
+  generate "blacklight:install", ENV.fetch("BLACKLIGHT_INSTALL_OPTIONS", "--devise --skip-solr")
+  generate "geoblacklight:install"
+  rails_command "db:migrate"
+end
