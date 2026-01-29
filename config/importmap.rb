c779375d965af15dc9c99031df455458197b5461
Nick Budak <budak@stanford.edu>
2026-01-28 16:29:35 -0800

    Use jsdelivr ESM URLs for importmap dependencies
    Also removes the pins for clover viewer stuff since we're not
    using the clover viewer anymore.
    
    Fixes #1720

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,15 +6,9 @@ pin "ol/", to: "https://esm.sh/ol@8.1.0/"
 pin "ol-pmtiles", to: "https://cdn.skypack.dev/ol-pmtiles@0.3.0"
 
 # Leaflet viewer
-pin "leaflet", to: "https://cdn.skypack.dev/leaflet@1.9.4"
-pin "esri-leaflet", to: "https://cdn.skypack.dev/esri-leaflet@3.0.12"
-pin "leaflet-fullscreen", to: "https://cdn.skypack.dev/leaflet-fullscreen@1.0.2"
-
-# Clover viewer
-pin "react", to: "https://esm.sh/react@18.3.1"
-pin "react-dom/client", to: "https://esm.sh/react-dom@18.3.1/client"
-pin "@samvera/clover-iiif/viewer", to: "https://esm.sh/@samvera/clover-iiif@2.10.0/dist/viewer"
-pin "@samvera/clover-iiif/image", to: "https://esm.sh/@samvera/clover-iiif@2.10.0/dist/image"
+pin "leaflet", to: "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet-src.esm.js"
+pin "esri-leaflet", to: "https://cdn.jsdelivr.net/npm/esri-leaflet@3.0.19/+esm"
+pin "leaflet.fullscreen", to: "https://cdn.jsdelivr.net/npm/leaflet.fullscreen@5.3.0/+esm"
 
 # Geoblacklight
 pin_all_from Geoblacklight::Engine.root.join("app", "javascript", "geoblacklight"), under: "geoblacklight"
