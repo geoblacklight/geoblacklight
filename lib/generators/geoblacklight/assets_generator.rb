a3d5a948b9f50c3751a4f28e7c4ee6f13bde0ef0
Nick Budak <budak@stanford.edu>
2026-05-27 12:22:07 -0700

    Make importmaps the default asset pipeline for new v5 apps
    This aligns with the recommendation we make in the v5 docs to
    use importmaps instead of vite.

--- lib/generators/geoblacklight/assets_generator.rb
+++ lib/generators/geoblacklight/assets_generator.rb
@@ -4,7 +4,7 @@ module Geoblacklight
   # Run general asset setup and then delegate to the appropriate generator
   # Based on Blacklight::AssetsGenerator
   class AssetsGenerator < Rails::Generators::Base
-    class_option :"asset-pipeline", type: :string, default: ENV.fetch("ASSET_PIPELINE", "vite"), desc: "Choose the asset pipeline to use (vite or importmap)"
+    class_option :"asset-pipeline", type: :string, default: ENV.fetch("ASSET_PIPELINE", "importmap"), desc: "Choose the asset pipeline to use (vite or importmap)"
     class_option :test, type: :boolean, default: ENV.fetch("CI", false), aliases: "-t", desc: "Indicates that app will be installed in a test environment"
 
     # Pick a version of the frontend asset package and install it.
@@ -27,7 +27,7 @@ module Geoblacklight
       generator = if options[:"asset-pipeline"]
         "geoblacklight:assets:#{options[:"asset-pipeline"]}"
       else
-        "geoblacklight:assets:vite"
+        "geoblacklight:assets:importmap"
       end
 
       generate generator, generated_options
