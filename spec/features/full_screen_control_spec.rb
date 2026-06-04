1b4a8d641effafeab60c01dce48cf9dce77f76b5
Justin Coyne <jcoyne@justincoyne.com>
2026-06-04 16:52:29 -0500

    Remove test for leaflet controls on a stanford item
    Stanford doesn't use leaflet

--- spec/features/full_screen_control_spec.rb
+++ spec/features/full_screen_control_spec.rb
@@ -2,13 +2,6 @@
 
 require "spec_helper"
 
-feature "Leaflet fullscreen control", js: true do
-  scenario "WMS layer should have full screen control" do
-    visit solr_document_path("stanford-cz128vq0535")
-    expect(page).to have_css(".leaflet-control-zoom-fullscreen")
-  end
-end
-
 feature "IIIF fullscreen control", js: true do
   scenario "IIIF layer should have full screen control" do
     visit solr_document_path("princeton-sx61dn82p")
