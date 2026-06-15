e0154ddb2c67d03077db407603f5d3b9c3861a7c
Justin Coyne <jcoyne@justincoyne.com>
2026-06-15 13:38:01 -0500

    Use FacetItemPresenter to generate proper paths
    Prior to this change, the links on the front page were using search_catalog_path, but the intended way in Blacklight to link to these searches is via the presenter. This removes the undesirable /catalog url path.
    
    Ref #1258

--- spec/components/geoblacklight/homepage_feature_facet_component_spec.rb
+++ spec/components/geoblacklight/homepage_feature_facet_component_spec.rb
@@ -4,34 +4,29 @@ require "spec_helper"
 
 RSpec.describe Geoblacklight::HomepageFeatureFacetComponent, type: :component do
   # Build a search
-  let(:context) { {whatever: :value} }
-  let(:service) { Blacklight::SearchService.new(config: blacklight_config, search_state:, **context) }
-  let(:repository) { Blacklight::Solr::Repository.new(blacklight_config) }
+  let(:service) { Blacklight::SearchService.new(config: blacklight_config, search_state:) }
   let(:search_state) { Blacklight::SearchState.new({}, blacklight_config) }
-  let(:blacklight_config) { Blacklight::Configuration.new }
-  let(:copy_of_catalog_config) { ::CatalogController.blacklight_config.deep_copy }
-  let(:blacklight_solr) { RSolr.connect(Blacklight.connection_config.except(:adapter)) }
+  let(:blacklight_config) { ::CatalogController.blacklight_config.deep_copy }
+  let(:facet_field) { Geoblacklight.configuration.fields.provider }
+  let(:facet_config) { blacklight_config.facet_fields[facet_field] }
+  let(:facet_field_presenter) { Blacklight::FacetFieldPresenter.new(facet_config, nil, vc_test_controller.view_context, nil) }
+  let(:response) { service.search_results }
+  let(:items) { response.aggregations[facet_field].items }
 
-  describe "homepage" do
-    before do
-      (@response, @document_list) = service.search_results
-    end
+  subject(:instance) do
+    described_class.new(
+      icon: "home",
+      label: "geoblacklight.home.institution",
+      facet_field_presenter: facet_field_presenter,
+      items: items
+    )
+  end
 
-    let(:kargs) do
-      {
-        icon: "home",
-        label: "geoblacklight.home.institution",
-        facet_field: Geoblacklight.configuration.fields.provider,
-        response: @response
-      }
-    end
-
-    it "includes facet links" do
-      render_inline(described_class.new(**kargs))
-      expect(page).to have_selector("div.category-block")
-      expect(page).to have_selector("div.category-icon")
-      expect(page).to have_selector("a.home-facet-link")
-      expect(page).to have_selector("a.more_facets_link")
-    end
+  it "includes facet links" do
+    render_inline(instance)
+    expect(page).to have_selector("div.category-block")
+    expect(page).to have_selector("div.category-icon")
+    expect(page).to have_selector("a.home-facet-link")
+    expect(page).to have_selector("a.more_facets_link")
   end
 end
