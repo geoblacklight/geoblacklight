4c7d0f3b1823a7cf26b42cecf75f057f8c7c87e9
Justin Coyne <jcoyne@justincoyne.com>
2026-06-15 13:38:01 -0500

    Use FacetItemPresenter to generate proper paths
    Prior to this change, the links on the front page were using search_catalog_path, but the intended way in Blacklight to link to these searches is via the presenter. This removes the undesirable /catalog url path.
    
    Ref #1258

--- app/components/geoblacklight/homepage_feature_facet_component.rb
+++ app/components/geoblacklight/homepage_feature_facet_component.rb
@@ -2,20 +2,20 @@
 
 module Geoblacklight
   class HomepageFeatureFacetComponent < ViewComponent::Base
-    def initialize(icon:, label:, facet_field:, response:)
+    def initialize(icon:, label:, facet_field_presenter:, items:)
       @icon = icon
       @label = label
-      @facet_field = facet_field
-      @items = response.aggregations[@facet_field].items
-
+      @facet_field_presenter = facet_field_presenter
+      @items = items
       super()
     end
 
     def facets
       links = @items.map do |item|
-        link_to(item.value, search_catalog_path("f[#{@facet_field}][]": item.value), class: "home-facet-link")
+        item_presenter = @facet_field_presenter.item_presenter(item)
+        link_to(item_presenter.value, item_presenter.href, class: "home-facet-link")
       end
-      links << link_to("more »", facet_catalog_path(@facet_field), class: "more_facets_link")
+      links << link_to("more »", facet_catalog_path(@facet_field_presenter.key), class: "more_facets_link")
       safe_join(links, ", ")
     end
   end
