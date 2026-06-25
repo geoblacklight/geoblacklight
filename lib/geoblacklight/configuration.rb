bef6de7c1811d6de3befc7d8d3fd9c791b31d175
Justin Coyne <jcoyne@justincoyne.com>
2026-06-25 08:23:21 -0500

    Add missing defaults to configuration

--- lib/geoblacklight/configuration.rb
+++ lib/geoblacklight/configuration.rb
@@ -95,6 +95,8 @@ module Geoblacklight
         ]
       }
 
+      @vector_download_formats = ["Shapefile", "KMZ", "GeoJSON", "CSV"]
+      @metadata_shown = ["mods", "fgdc", "iso19139", "html"]
       @webservices_shown = %w[
         wms
         tms
@@ -133,6 +135,8 @@ module Geoblacklight
           note_prefix: "Warning: "
         )
       }
+
+      @sidebar_static_map = ["iiif", "iiif_manifest"]
     end
 
     def fields
