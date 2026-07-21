118c07bf81c7c1c9761568374a6185bbc3a416b6
Justin Coyne <jcoyne@justincoyne.com>
2026-07-21 14:48:16 -0500

    Convert icons into badges
    Fixes #1849

--- app/components/geoblacklight/header_icons_component.rb
+++ app/components/geoblacklight/header_icons_component.rb
@@ -12,20 +12,16 @@ module Geoblacklight
       super()
     end
 
-    def icon(field)
-      return resource_icon if field == Geoblacklight.configuration.fields.resource_class
-
-      helpers.geoblacklight_icon(@document[field], classes: "svg_tooltip")
+    def render_badge(field)
+      render badge_class(field).new(document: @document, field:)
     end
 
-    # If the item has a resource type of "X data" where "X" is an existing icon, use that icon
-    # Otherwise just use the resource class icon
-    def resource_icon
-      dataset_type = @document.resource_type.find { |type| type.include?(" data") }&.gsub(" data", "")
-      resource_type_icon = helpers.geoblacklight_icon(dataset_type, classes: "svg_tooltip")
-      return resource_type_icon unless resource_type_icon.include?("icon-missing")
-
-      helpers.geoblacklight_icon(@document.resource_class.first, classes: "svg_tooltip")
+    def badge_class(field)
+      if field == Geoblacklight.configuration.fields.resource_class
+        Geoblacklight::ResourceHeaderBadgeComponent
+      else
+        Geoblacklight::HeaderBadgeComponent
+      end
     end
   end
 end
